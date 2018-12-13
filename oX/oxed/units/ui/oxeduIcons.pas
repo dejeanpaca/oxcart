2dab7e48001d584850cd967330d30e4c1b63232d
Dejan Boras <de.jean.7777@protonmail.com>
2018-12-13 21:52:13 +0100

    oxeduIcons now uses oxuGlyph to handle icon textures

--- oX/oxed/units/ui/oxeduIcons.pas
+++ oX/oxed/units/ui/oxeduIcons.pas
@@ -14,32 +14,23 @@ INTERFACE
       uStd, StringUtils,
       {ox}
       uOX, oxuPaths, oxuFreetype, oxuTexture, oxuTexturePool, uiuContextMenu, oxuDefaultTexture,
+      oxuGlyph,
       {oxed}
       uOXED, oxuFileIcons;
 
 TYPE
-   oxedTIconsSource = record
-      Font: oxTFreetypeFont;
-      Textures: oxTTexturePool;
-   end;
-
    { oxedTIconsGlobal }
 
    oxedTIconsGlobal = record
-      Primary,
-      Secondary: oxedTIconsSource;
       {default size for glyphs}
       DefaultSize: loopint;
       {will return glyph even if one cannot be loaded due to issues (to prevent any potential crashes)}
       AlwaysReturnGlyphs: boolean;
 
-      function Create(c: longword; size: longint; const name: string; const source: oxedTIconsSource): oxTTexture;
       function Create(c: longword; size: longint = 0; const name: string = ''): oxTTexture;
-      function CreateSecondary(c: longword; size: longint = 0; const name: string = ''): oxTTexture;
       function Create(const c: string; size: longint = 0): oxTTexture;
 
       procedure Create(item: uiPContextMenuItem; c: longword; size: longint = 0);
-      procedure CreateSecondary(item: uiPContextMenuItem; c: longword; size: longint = 0);
    end;
 
 VAR
@@ -49,19 +40,9 @@ IMPLEMENTATION
 
 procedure init();
 var
-   path: UTF8String;
    tex: oxTTexture;
 
 begin
-   path := oxAssetPaths.Find(oxPaths.Fonts + 'FontAwesome.otf');
-   oxedIcons.Primary.Font := oxFreetypeManager.Load('default', path);
-
-   path := oxAssetPaths.Find(oxPaths.Fonts + 'MaterialIcons-Regular.woff');
-   oxedIcons.Secondary.Font := oxFreetypeManager.Load('material', path);
-
-   oxedIcons.Primary.Textures := oxTTexturePool.Create();
-   oxedIcons.Secondary.Textures := oxTTexturePool.Create();
-
    {file}
    tex := oxedIcons.Create('file');
    oxFileIcons.SetFile(tex);
@@ -116,63 +97,28 @@ end;
 
 procedure deinit();
 begin
-   FreeObject(oxedIcons.Primary.Textures);
-   FreeObject(oxedIcons.Secondary.Textures);
+
 end;
 
 { oxedTIconsGlobal }
 
-function oxedTIconsGlobal.Create(c: longword; size: longint; const name: string; const source: oxedTIconsSource): oxTTexture;
+function oxedTIconsGlobal.Create(c: longword; size: longint; const name: string): oxTTexture;
 var
-   monospaced,
-   exactSize: boolean;
-   alphaType: oxTFreetypeAlphaType;
+   map: oxPGlyphMap;
+   codeName: string;
 
 begin
-   Result := oxTTexture(source.Textures.FindByPath('f' + sf(c)));
+   if(name = '') then begin
+      codeName := sf(c);
+      map := oxGlyphs.Load(codeName, codeName, size);
 
-   if(Result = nil)  then begin
-      if(source.Font <> nil) then begin
-         if(size = 0) then
-            size := DefaultSize;
+      if(AlwaysReturnGlyphs) and (map^.Texture = nil) then
+         exit(oxDefaultTexture.Texture);
 
-         monospaced := source.Font.Square;
-         exactSize := source.Font.ExactSize;
-         alphaType := source.Font.AlphaType;
-
-         source.Font.Square := true;
-         source.Font.ExactSize := true;
-         source.Font.AlphaType := oxFREETYPE_ALPHA_AVERAGE;
-
-         if(name = '') then
-            source.Font.CreateGlyphTexture(c, Result, size)
-         else
-            source.Font.CreateGlyphTexture(name, Result, size);
-
-         if(Result <> nil) then begin
-            Result.Path := 'f' + sf(c);
-            source.Textures.AddResource(Result);
-         end;
-
-         source.Font.Square := monospaced;
-         source.Font.ExactSize := exactSize;
-         source.Font.AlphaType := alphaType;
-      end else if(AlwaysReturnGlyphs) then
-         Result := oxDefaultTexture.Texture;
+      exit(map^.Texture);
    end;
 
-   if(Result <> nil) then
-      Result.MarkUsed();
-end;
-
-function oxedTIconsGlobal.Create(c: longword; size: longint; const name: string): oxTTexture;
-begin
-   Result := Create(c, size, name, Primary);
-end;
-
-function oxedTIconsGlobal.CreateSecondary(c: longword; size: longint; const name: string): oxTTexture;
-begin
-   Result := Create(c, size, name, Secondary);
+   Result := nil;
 end;
 
 function oxedTIconsGlobal.Create(const c: string; size: longint): oxTTexture;
@@ -186,17 +132,6 @@ var
 
 begin
    tex := Create(c, size);
-
-   if(tex <> nil) then
-      item^.SetGlyph(tex);
-end;
-
-procedure oxedTIconsGlobal.CreateSecondary(item: uiPContextMenuItem; c: longword; size: longint);
-var
-   tex: oxTTexture;
-
-begin
-   tex := CreateSecondary(c, size);
 
    if(tex <> nil) then
       item^.SetGlyph(tex);
