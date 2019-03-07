36fb142afb2d6b892a2acb56d8b73bbe6d681d01
Dejan Boras <de.jean.7777@protonmail.com>
2019-03-07 22:38:59 +0100

    Unit to keep UI settings

---
+++ oX/units/ui/uiuSettings.pas
@@ -0,0 +1,49 @@
+{
+   uiuFiles, file related ui stuff
+   Copyright (C) 2018. Dejan Boras
+
+   Started On:    13.12.2018.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT uiuSettings;
+
+INTERFACE
+
+   USES
+      udvars, uStd,
+      {ox}
+      oxuUI;
+
+TYPE
+
+   { uiTSettings }
+
+   uiTSettings = record
+      NaturalScroll: boolean;
+
+      function ScrollDirection(): loopint;
+   end;
+
+VAR
+  uiSettings: uiTSettings;
+
+IMPLEMENTATION
+
+VAR
+   dvNaturalScroll: TDVar;
+
+{ uiTSettings }
+
+function uiTSettings.ScrollDirection(): loopint;
+begin
+   if(not NaturalScroll) then
+      Result := 1
+   else
+      Result := -1;
+end;
+
+INITIALIZATION
+   oxTUI.dvg.Add(dvNaturalScroll, 'natural_Scroll', dtcBOOL, @uiSettings.NaturalScroll);
+
+END.
