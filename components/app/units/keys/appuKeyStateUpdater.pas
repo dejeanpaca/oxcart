6801322f8548256295fe197c7a7ac1e5c9c1f7b8
Dejan Boras <de.jean.7777@protonmail.com>
2019-04-11 21:31:39 +0200

    Separate key state updater into its own unit

---
+++ components/app/units/keys/appuKeyStateUpdater.pas
@@ -0,0 +1,30 @@
+{
+   appuKeys, keys
+   Copyright (C) 2007. Dejan Boras
+
+   Started On:    01.05.2007.
+}
+
+{$MODE OBJFPC}{$H+}{$MODESWITCH ADVANCEDRECORDS}
+UNIT appuKeyStateUpdater;
+
+INTERFACE
+
+   USES
+      appuKeys, appuRun, oxuRun;
+
+IMPLEMENTATION
+
+VAR
+   runRoutine: appTRunRoutine;
+
+procedure run();
+begin
+   appk.UpdateCycle();
+end;
+
+
+INITIALIZATION
+   oxRun.AddPreRoutine(runRoutine, 'app.keystateupdater', @run);
+
+END.
