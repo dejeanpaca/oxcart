8ae6dc29cc205342db8f2954f941ed5862f403c6
Dejan Boras <de.jean.7777@protonmail.com>
2019-05-20 22:05:51 +0200

    Unit for additional build events functionality
    - Cleanup project on overwrite

---
+++ oX/oxed/units/oxeduBuildEvents.pas
@@ -0,0 +1,31 @@
+{
+   oxeduBuildEvents, oxed build events
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    20.05.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxeduBuildEvents;
+
+INTERFACE
+
+   USES
+      appuActionEvents,
+      {oxed}
+      oxeduActions, oxeduBuild, oxeduProjectManagement;
+
+IMPLEMENTATION
+
+{NOTE: cleanup project after a overwrite, because we don't want to encounter problems with possible
+ leftovers of a previous project}
+procedure overwriteCleanup();
+begin
+   appActionEvents.Queue(oxedActions.CLEANUP);
+end;
+
+
+INITIALIZATION
+   oxedProjectManagement.OnProjectOverwritten.Add(@overwriteCleanup);
+
+END.
