e5cf04ceab2733bf646dd3e3da593b53aa25eba8
Dejan Boras <de.jean.7777@protonmail.com>
2019-06-10 21:27:35 +0200

    Clean up android code, and remove dead code

--- oX/units/platforms/android/oxuAndroidPlatform.pas
+++ oX/units/platforms/android/oxuAndroidPlatform.pas
@@ -11,38 +11,87 @@ UNIT oxuAndroidPlatform;
 INTERFACE
 
    USES
-      oxuTypes, oxuPlatform, oxuWindowTypes;
-
-
-IMPLEMENTATION
+      uStd,
+      {app}
+      appuMouse,
+      {oX}
+      oxuTypes, oxuPlatform, oxuPlatforms, oxuWindowTypes;
 
 TYPE
    { oxTAndroidPlatform }
 
-   TAndroidPlatform = class(oxTPlatform)
-      constructor Create; override;
+   oxTAndroidPlatform = class(oxTPlatform)
+      constructor Create(); override;
+      function Initialize(): boolean; override;
 
-      function MakeWindow(wnd: oxTWindow): boolean; override;
+      function MakeWindow({%H-}wnd: oxTWindow): boolean; override;
    end;
 
-{ oxTAndroidPlatform }
+   { TAndroidPointerDriver }
 
-constructor TAndroidPlatform.Create;
+   TAndroidPointerDriver = class(appTPointerDriver)
+      LastX,
+      LastY: loopint;
+
+      constructor Create();
+
+      procedure getXY({%H-}devID: longint; wnd: pointer; out x, y: single); override;
+      procedure setXY({%H-}devID: longint; wnd: pointer; x, y: single); override;
+      procedure hide(devID: longint; {%H-}wnd: pointer); override;
+      procedure show(devID: longint; {%H-}wnd: pointer); override;
+   end;
+
+
+IMPLEMENTATION
+
+{ TAndroidPointerDriver }
+
+constructor TAndroidPointerDriver.Create();
 begin
    Name := 'android';
 end;
 
-function TAndroidPlatform.MakeWindow(wnd: oxTWindow): boolean;
+procedure TAndroidPointerDriver.getXY(devID: longint; wnd: pointer; out x, y: single);
 begin
-   result := true;
+   inherited getXY(devID, wnd, x, y);
 end;
 
-procedure setPlatform();
+procedure TAndroidPointerDriver.setXY(devID: longint; wnd: pointer; x, y: single);
 begin
-   oxPlatform := TAndroidPlatform.Create();
+   inherited setXY(devID, wnd, x, y);
+end;
+
+procedure TAndroidPointerDriver.hide(devID: longint; wnd: pointer);
+begin
+   inherited hide(devID, wnd);
+end;
+
+procedure TAndroidPointerDriver.show(devID: longint; wnd: pointer);
+begin
+   inherited show(devID, wnd);
+end;
+
+{ oxTAndroidPlatform }
+
+constructor oxTAndroidPlatform.Create();
+begin
+   Name := 'android';
+end;
+
+function oxTAndroidPlatform.Initialize(): boolean;
+begin
+   { pointer driver }
+   PointerDriver := TAndroidPointerDriver.Create();
+
+   Result := true;
+end;
+
+function oxTAndroidPlatform.MakeWindow(wnd: oxTWindow): boolean;
+begin
+   Result := true;
 end;
 
 INITIALIZATION
+   oxPlatforms.Register(oxTAndroidPlatform);
 
-   oxonSetPlatform := @setPlatform;
 END.
