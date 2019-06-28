79ddfb21ed3b476779bde51dafd260992dc5a909
Dejan Boras <de.jean.7777@protonmail.com>
2019-06-28 12:29:41 +0200

    oX system information unit to handle library mode without reading sytem information again

---
+++ oX/units/oxuSysInfo.pas
@@ -0,0 +1,48 @@
+{
+   appuSysInfo, provides system information
+   Copyright (C) 2011. Dejan Boras
+
+   Started On:    27.03.2011.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuSysInfo;
+
+INTERFACE
+
+   USES
+      {$IFNDEF OX_LIBRARY}
+      appuSysInfo, {$IFDEF OX_LIBRARY_SUPPORT}appuSysInfoBase, oxuGlobalInstances{$ENDIF};
+      {$ELSE}
+      uApp, appuSysInfoBase, oxuGlobalInstances,
+      uOX, oxuRunRoutines;
+      {$ENDIF}
+
+IMPLEMENTATION
+
+{$IFDEF OX_LIBRARY}
+VAR
+   initRoutines: oxTRunRoutine;
+
+procedure initialize();
+var
+   instance: appPSystemInformation;
+
+begin
+   instance := oxExternalGlobalInstances.FindInstancePtr('appTSystemInformation');
+
+   if(instance <> nil) then
+      appSI := instance^;
+end;
+{$ENDIF}
+
+INITIALIZATION
+   {$IFDEF OX_LIBRARY_SUPPORT}
+   oxGlobalInstances.Add('appTSystemInformation', @appSI);
+   {$ENDIF}
+
+   {$IFDEF OX_LIBRARY}
+   app.InitializationProcs.Add(initRoutines, 'ox.system_information', @initialize);
+   {$ENDIF}
+
+END.
