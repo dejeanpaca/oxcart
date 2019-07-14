a6788b1f07ac651fd2151904ddf913e2536cc8a6
Dejan Boras <de.jean.7777@protonmail.com>
2019-07-14 23:42:10 +0200

    Update the gltransform test

--- oX/tests/basic/gltransform.pas
+++ oX/tests/basic/gltransform.pas
@@ -96,17 +96,19 @@ begin
 
    wnd := oxWindow.Current;
 
-   projections[0].Initialize(projections[0]);
+   oxTProjection.CreateFromWindow(projections[0], oxWindow.Current);
    projections[0].Name := 'ox';
    projections[0].ClearColor.Assign(0.2, 0.2, 1.0, 1.0);
    projections[0].SetViewport(0, 0, wnd.Dimensions.w div 2, wnd.Dimensions.h);
-   projections[0].Perspective(60, 0.5, 1000.0);
+//   projections[0].SetViewportf(0, 0, 0.5, 1.0);
+   projections[0].DefaultPerspective();
 
-   projections[1].Initialize(projections[1]);
+   oxTProjection.CreateFromWindow(projections[1], oxWindow.Current);
    projections[1].Name := 'gl';
    projections[1].ClearColor.Assign(1.0, 0.2, 0.2, 1.0);
    projections[1].SetViewport(wnd.Dimensions.w div 2, 0, wnd.Dimensions.w div 2, wnd.Dimensions.h);
-   projections[1].Perspective(60, 0.5, 1000.0);
+   //projections[1].SetViewportf(0.5, 0, 0.5, wnd.Dimensions.h);
+   projections[1].DefaultPerspective();
 
    wnd.Projection.Enabled := false;
 
