cd4036addb09a3fe87a5f9e86c3dc80c92b2d3b9
Dejan Boras <de.jean.7777@protonmail.com>
2019-09-10 12:32:45 +0200

    Materials now have a resource loader (wip)

---
+++ oX/units/oxuMaterialLoader.pas
@@ -0,0 +1,61 @@
+{
+   oxuMaterialLoader, material resource loader
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    10.09.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuMaterialLoader;
+
+INTERFACE
+
+   USES
+      uStd, uLog,
+      {oX}
+      uOX, oxuTypes,
+      oxuMaterial, oxuMaterialFile, oxuResourceLoader;
+
+TYPE
+
+   { oxTMaterialResourceLoader }
+
+   oxTMaterialResourceLoader = object(oxTResourceLoader)
+      constructor Create();
+
+      function Load(resource: oxTResource): boolean; reintroduce;
+   end;
+
+VAR
+   oxMaterialResourceLoader: oxTMaterialResourceLoader;
+
+IMPLEMENTATION
+{ oxTMaterialResourceLoader }
+
+constructor oxTMaterialResourceLoader.Create();
+begin
+   Name := 'Material';
+end;
+
+function oxTMaterialResourceLoader.Load(resource: oxTResource): boolean;
+var
+   material: oxTMaterial;
+
+begin
+   Result := false;
+   material := oxTMaterial(resource);
+
+   if(material.Path <> '') then begin
+      log.v('Loading material: ' + material.Path);
+
+      // TODO: Load material
+   end;
+
+   // TODO: Load textures
+end;
+
+INITIALIZATION
+   oxMaterialResourceLoader.Create();
+   oxMaterial.ResourceLoader := @oxMaterialResourceLoader;
+
+END.
