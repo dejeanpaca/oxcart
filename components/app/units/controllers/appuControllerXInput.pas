3d73c4fed5e650b9cf93886cd3dba4b79f271f49
Dejan Boras <de.jean.7777@protonmail.com>
2019-09-23 12:24:39 +0200

    Some small improvements to XInput support

--- components/app/units/controllers/appuControllerXInput.pas
+++ components/app/units/controllers/appuControllerXInput.pas
@@ -11,28 +11,38 @@ UNIT appuControllerXInput;
 INTERFACE
 
    USES
-      uStd, uLog,
+      uStd, uLog, StringUtils,
       appuController,
-      DX12.xinput;
+      {windows}
+      windows, DX12.xinput;
 
 TYPE
 
    { appTXInputControllerHandler }
 
    appTXInputControllerHandler = class(appTControllerHandler)
+      public
+         {XInput specific properties}
+
+         {identifier}
+         GUID: string;
+         {is the controller a game controller (gamepad, joystick)}
+         GameController: boolean;
+
       procedure Initialize(); override;
       procedure Reset(); override;
       procedure Run(); override;
 
       private
-         procedure Add(const fn: string);
+         function Add(index: loopint): boolean;
    end;
 
    { appTXInputControllerDevice }
 
    appTXInputControllerDevice = class(appTControllerDevice)
-      FileName: string;
+      XInputIndex: loopint;
 
+      procedure Initialize(index: loopint; var capabilities: TXINPUT_CAPABILITIES);
       procedure Run(); override;
       procedure DeInitialize(); override;
    end;
@@ -41,8 +51,29 @@ IMPLEMENTATION
 
 { appTXInputControllerDevice }
 
-procedure appTXInputControllerDevice.Run();
+procedure appTXInputControllerDevice.Initialize(index: loopint; var capabilities: TXINPUT_CAPABILITIES);
 begin
+   XInputIndex := index;
+   ButtonCount := PopCnt(capabilities.Gamepad.wButtons);
+
+   if(capabilities.SubType = XINPUT_DEVSUBTYPE_GAMEPAD) then begin
+      AxisCount := 4;
+      TriggerCount := 2;
+      HatCount := 1;
+   end else
+      Valid := false;
+
+   Name := 'XInput ' + sf(index);
+end;
+
+procedure appTXInputControllerDevice.Run();
+var
+   xstate: TXINPUT_STATE;
+
+begin
+   XInputGetState(XInputIndex, xstate);
+
+   // TODO: Process state
 end;
 
 procedure appTXInputControllerDevice.DeInitialize();
@@ -58,8 +89,20 @@ begin
 end;
 
 procedure appTXInputControllerHandler.Reset();
+var
+   i: loopint;
+
 begin
    inherited;
+
+   for i := 0 to 3 do begin
+      if(not Add(i)) then begin
+         if(i = 0) then
+            log.v('No XInput devices detected');
+
+         break;
+      end;
+   end;
 end;
 
 procedure appTXInputControllerHandler.Run();
@@ -67,16 +110,27 @@ begin
    inherited Run();
 end;
 
-procedure appTXInputControllerHandler.Add(const fn: string);
+function appTXInputControllerHandler.Add(index: loopint): boolean;
 var
    device: appTXInputControllerDevice;
+   capabilities: TXINPUT_CAPABILITIES;
 
 begin
-   device := appTXInputControllerDevice.Create();
+   if(XInputGetCapabilities(index, 0, capabilities) = ERROR_SUCCESS) then begin
+      device := appTXInputControllerDevice.Create();
 
-   // device.Initialize(fn);
+      device.Initialize(index, capabilities);
 
-   appControllers.Add(device);
+      appControllers.Add(device);
+
+      log.Collapsed(device.Name);
+      device.LogDevice();
+      log.Leave();
+
+      exit(true);
+   end;
+
+   Result := false;
 end;
 
 INITIALIZATION
