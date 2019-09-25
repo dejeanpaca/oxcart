5ed7ee43c84ac0fe36d841f93f739b5f4943ac53
Dejan Boras <de.jean.7777@protonmail.com>
2019-09-25 12:21:52 +0200

    Start working on a window to display controller information and test

---
+++ oX/units/ui/wnd/oxuwndInputControllerInfo.pas
@@ -0,0 +1,25 @@
+{
+   oxuwndInputControllerInfo, controller information/test window
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    25.09.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuwndInputControllerInfo;
+
+INTERFACE
+
+   USES
+      uStd,
+      {app}
+      uApp, appuController,
+      {ox}
+      uOX, oxuTypes, oxuwndSettings,
+      {ui}
+      uiWidgets, wdguLabel, wdguButton, wdguDivisor;
+
+
+IMPLEMENTATION
+
+END.
