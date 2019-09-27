6fdac24174297af7c4d77326796a229e02e0038e
Dejan Boras <de.jean.7777@protonmail.com>
2019-09-27 12:46:54 +0200

    No need for a separate IsSystemDarkTheme() method

--- oX/units/ui/uiuPlatform.pas
+++ oX/units/ui/uiuPlatform.pas
@@ -17,9 +17,6 @@ TYPE
    { uiTPlatformComponent }
 
    uiTPlatformComponent = class
-      {Check if the system is currently using a dark theme.
-      Returns 0 for no, 1 for yes, and -1 if unknown}
-      function IsSystemDarkTheme(): loopint; virtual;
       {get the name of the system theme}
       function GetSystemTheme(): StdString; virtual;
    end;
@@ -27,11 +24,6 @@ TYPE
 IMPLEMENTATION
 
 { uiTPlatformComponent }
-
-function uiTPlatformComponent.IsSystemDarkTheme(): loopint;
-begin
-   Result := -1;
-end;
 
 function uiTPlatformComponent.GetSystemTheme(): StdString;
 begin
