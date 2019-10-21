47c81b2e2516e028554ab688d4088e6119fa419e
Dejan Boras <de.jean.7777@protonmail.com>
2019-10-21 22:39:49 +0200

    Unit to determine gl features
    - Currently determines if gl supports non pow2 textures

---
+++ oX/units/renderers/gl/oxuglFeatures.pas
@@ -0,0 +1,49 @@
+{
+   oglFeatures, check opengl features
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    21.10.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuglFeatures;
+
+INTERFACE
+
+   USES
+      uLog, StringUtils,
+      {ox}
+      uOX, oxuRunRoutines,
+      {gl}
+      oxuglRenderer, oxuOGL, oxuglExtensions;
+
+IMPLEMENTATION
+
+VAR
+   glInitRoutines: oxTRunRoutine;
+
+procedure getFeatures();
+var
+   glr: oxglTRenderer;
+
+begin
+   glr := oxglRenderer;
+
+   glr.Properties.Textures.NonPowerOf2 := oglExtensions.Supported(cGL_ARB_texture_non_power_of_two);
+   log.v('Supports non power of two textures: ' + sf(glr.Properties.Textures.NonPowerOf2));
+end;
+
+procedure initGl();
+begin
+   getFeatures();
+end;
+
+procedure init();
+begin
+   oxglRenderer.AfterInit.Add(glInitRoutines, 'gl.fpshader', @initGl);
+end;
+
+INITIALIZATION
+   ox.PreInit.Add('ox.gl.features', @init);
+
+END.
