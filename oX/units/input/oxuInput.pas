ffcdca352756e440612d982ed74a2700a7570054
Dejan Boras <de.jean.7777@protonmail.com>
2019-10-28 12:49:42 +0100

    Start implementing a input unit for unified input control

---
+++ oX/units/input/oxuInput.pas
@@ -0,0 +1,20 @@
+{
+   oxuInput, oX unified input
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    28.10.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuInput;
+
+INTERFACE
+
+   USES
+      appuKeys, appuMouse,
+      oxuKeyboardControl,
+      appuController;
+
+IMPLEMENTATION
+
+END.
