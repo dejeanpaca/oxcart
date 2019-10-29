f0c215b9f7015fe9776fd635d1ebf80a603da2ee
Dejan Boras <de.jean.7777@protonmail.com>
2019-10-29 21:01:08 +0100

    Expand OXED project statistics functionality

--- oX/oxed/units/project/oxeduProjectStatistics.pas
+++ oX/oxed/units/project/oxeduProjectStatistics.pas
@@ -16,18 +16,35 @@ INTERFACE
       oxeduProjectManagement;
 
 TYPE
+   oxedPFileTypeStatistics = ^oxedTFileTypeStatistics;
+
    oxedTFileTypeStatistics = record
       Extension: string;
-      FileCount: loopint;
+      {file count for this extension}
+      Count,
+      {total size of files for this extension}
+      TotalSize: loopint;
    end;
 
    oxedTFileTypeStatisticsList = specialize TSimpleList<oxedTFileTypeStatistics>;
 
+   { oxedTFileTypeStatisticsListHelper }
+
+   oxedTFileTypeStatisticsListHelper = record helper for oxedTFileTypeStatisticsList
+      {add new file type by extension}
+      procedure Add(const ext: string);
+      {find file type by extension}
+      function FindByExtension(const ext: string): loopint;
+   end;
+
    { oxedTProjectStatistics }
 
    oxedTProjectStatistics = record
+      {total files in project}
       FileCount,
+      {total size of files in project}
       TotalSize: loopint;
+      {statistics per file type (by extension)}
       FileTypes: oxedTFileTypeStatisticsList;
 
       procedure Reset();
@@ -37,6 +54,33 @@ VAR
    oxedProjectStatistics: oxedTProjectStatistics;
 
 IMPLEMENTATION
+
+{ oxedTFileTypeStatisticsListHelper }
+
+procedure oxedTFileTypeStatisticsListHelper.Add(const ext: string);
+var
+   f: oxedTFileTypeStatistics;
+
+begin
+   ZeroPtr(@f, SizeOf(f));
+
+   f.Extension := ext;
+   f.Count := 1;
+end;
+
+function oxedTFileTypeStatisticsListHelper.FindByExtension(const ext: string): loopint;
+var
+   i: loopint;
+
+begin
+   for i := 0 to n - 1 do begin
+      if(List[i].Extension = ext) then begin
+         exit(i);
+      end;
+   end;
+
+   Result := -1;
+end;
 
 { oxedTProjectStatistics }
 
@@ -56,5 +100,7 @@ end;
 INITIALIZATION
    oxedProjectManagement.OnOpen.Add(@reset);
    oxedProjectManagement.OnClosed.Add(@reset);
+
+   oxedProjectStatistics.FileTypes.Initialize(oxedProjectStatistics.FileTypes, 128);
 
 END.
