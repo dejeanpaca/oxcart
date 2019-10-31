e8700b71d0ec1d493254b65395e11238ec55937d
Dejan Boras <de.jean.7777@protonmail.com>
2019-10-31 23:46:48 +0100

    Expand oxuInput structures

--- oX/units/input/oxuInput.pas
+++ oX/units/input/oxuInput.pas
@@ -11,9 +11,38 @@ UNIT oxuInput;
 INTERFACE
 
    USES
+      uStd,
       appuKeys, appuMouse,
-      oxuKeyboardControl,
-      appuController;
+      appuController,
+      {oX}
+      oxuKeyboardControl;
+
+TYPE
+   oxTInputDeviceType = (
+      OX_INPUT_DEVICE_KEYBOARD,
+      OX_INPUT_DEVICE_POINTER,
+      OX_INPUT_DEVICE_CONTROLLER
+   );
+
+   oxTInputActionMapping = record
+      DeviceType: oxTInputDeviceType;
+   end;
+
+   oxTInputAction = record
+      {action Id}
+      ActionId,
+      {how many inputs are mapped to this action}
+      MappingCount: loopint;
+
+      {all mappings related to this action}
+      Mappings = array[0..3] of oxTInputActionMapping;
+   end;
+
+   oxTInputActionsList = specialize TSimpleList<oxTInputAction>;
+
+   oxTInputActions = record
+     List: oxTInputActionsList;
+   end;
 
 IMPLEMENTATION
 
