896525d2db2b71df54fcb69b874865c9568b560c
Dejan Boras <de.jean.7777@protonmail.com>
2019-12-02 21:54:47 +0100

    Improve scene view action behavior

--- oX/oxed/units/ui/oxeduViewScene.pas
+++ oX/oxed/units/ui/oxeduViewScene.pas
@@ -12,6 +12,8 @@ INTERFACE
 
    USES
       vmVector, appuActionEvents,
+      {ox}
+      oxuCamera,
       {oxed}
       oxeduActions, oxeduSceneWindow;
 
@@ -24,44 +26,49 @@ var
 
 begin
    wnd := oxedSceneWindows.LastSelectedWindow;
+
    if(wnd <> nil) and (wnd.ControlCamera) then begin
       distance := wnd.Camera.vPos.Distance(vmvZero3f);
 
-      wnd.Camera.vPos := vmvZero3f + (pview * -distance);
+      wnd.Camera.PitchYaw(pview[0], pview[1]);
 
-      wnd.Camera.vView := pview;
-      wnd.Camera.vView.Normalize();
-
-      wnd.Camera.UpFromView();
+      wnd.Camera.vPos := wnd.Camera.vView * -distance;
    end;
 end;
 
 procedure viewBack();
 begin
-   view(vmCreate(0, 0, 1.0));
+   view(vmCreate(0, 0, 0.0));
 end;
 
 procedure viewFront();
 begin
-   view(vmCreate(0, 0, -1.0));
+   view(vmCreate(180, 180, 0.0));
 end;
 procedure viewLeft();
 begin
-   view(vmCreate(1.0, 0, 0));
+   view(vmCreate(0, 90, 0.0));
 end;
 procedure viewRight();
 begin
-   view(vmCreate(-1.0, 0, 0));
+   view(vmCreate(0, -90, 0.0));
 end;
 
 procedure viewUp();
+var
+   wnd: oxedTSceneWindow;
+
 begin
-   view(vmCreate(0, -1.0, 0));
+   wnd := oxedSceneWindows.LastSelectedWindow;
+
+   if(wnd <> nil) and (wnd.ControlCamera) then begin
+      view(vmCreate(-90, 0, 0.0));
+   end;
 end;
 
 procedure viewDown();
 begin
-   view(vmCreate(0, 1.0, 0));
+   view(vmCreate(90, 0, 0.0));
 end;
 
 INITIALIZATION
