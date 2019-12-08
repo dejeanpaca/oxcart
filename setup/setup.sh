56756c45e6b02a0a7331d3164e6bde0db5e97614
Dejan Boras <de.jean.7777@protonmail.com>
2019-12-08 22:12:31 +0100

    Check if lazbuild is present in path before executing setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,18 @@
-#!/usr/bin/env bash
+	#!/usr/bin/env bash
 
 # make sure we're always in the correct directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
+
+# check if we have lazbuild present
+lbpath=$(which lazbuild)
+
+if [[ -x "$lbpath" ]]; then
+	echo "lazbuild present"
+else
+	echo "Could not find lazbuild. Make sure it is in your PATH."
+	exit
+fi
 
 # compile setup
 lazbuild setup.lpi
