4e5e6bbcb9419ab3d52468043fa42347d06e3830
Dejan Boras <de.jean.7777@protonmail.com>
2020-01-10 23:07:31 +0100

    Fix: Don't use where in setup.cmd
    - This seems to fail when PATH is set for some reason (windows 10)

--- setup/setup.cmd
+++ setup/setup.cmd
@@ -1,13 +1,23 @@
 @echo off
 
 REM Check if we have lazbuild
-@where /q lazbuild
+@lazbuild -v >nul 2>&1
 
 if %ERRORLEVEL% EQU 0 (
     echo "Found lazbuild"
 ) else (
-    echo "Could not find lazbuild, check if it is set in your PATH."
-    EXIT /B
+    REM Set default path for lazarus
+    SET PATH="%PATH%C:\lazarus"
+
+    REM Check if we have lazbuild again
+    @lazbuild -v >nul 2>&1
+
+    if %ERRORLEVEL% EQU 0 (
+        echo "Lazbuild seems present"
+    ) else (
+        echo "Could not find lazbuild, check if it is set in your PATH."
+        EXIT /B
+    )
 )
 
 @lazbuild setup.lpi
