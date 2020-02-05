0336770d752c2ef978d5c185c7cf0c9b7379aa72
Dejan Boras <de.jean.7777@protonmail.com>
2020-02-05 21:28:17 +0100

    uFileTrash has been improved
    - TFileTrash no longer has a static method
    - The above allows us to store a LastError system code
    - winos functionality is implemented

--- units/common/uFileTrash.pas
+++ units/common/uFileTrash.pas
@@ -11,27 +11,51 @@ UNIT uFileTrash;
 INTERFACE
 
    USES
-      uStd, uFileUtils;
+      uStd, uFileUtils
+      {$IFDEF WINDOWS}
+      , ShellApi
+      {$ENDIF};
 
 TYPE
 
    { TFileTrash }
 
    TFileTrash = record
-      class procedure Recycle(const fn: StdString); static;
+      LastError: loopint;
+
+      function Recycle(const fn: StdString): boolean;
    end;
+
+VAR
+   FileTrash: TFileTrash;
 
 IMPLEMENTATION
 
 { TFileTrash }
 
 {$IFDEF WINDOWS}
-class procedure TFileTrash.Recycle(const fn: StdString);
-begin
+function TFileTrash.Recycle(const fn: StdString): boolean;
+VAR
+   fileop: SHFILEOPSTRUCTW;
+   from: array[0..4095] of WideChar;
 
+begin
+   ZeroOut(fileop, SizeOf(fileop));
+
+   from := StringToWideChar(fn + #0, @from[0], Length(from));
+
+   fileop.wFunc := FO_DELETE;
+   fileop.pFrom := @from[0];
+   fileop.fFlags := FOF_ALLOWUNDO or FOF_NOCONFIRMATION or FOF_NOERRORUI or FOF_SILENT;
+
+   LastError := SHFileOperationW(@fileop);
+   if(LastError = 0) then
+      exit(True);
+
+   Result := False;
 end;
 {$ELSE}
-class procedure TFileTrash.Recycle(const fn: StdString);
+procedure TFileTrash.Recycle(const fn: StdString);
 begin
 
 end;
