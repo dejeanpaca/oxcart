609d2392224a31cefcf05a6702c16d7c68296791
Dejan Boras <de.jean.7777@protonmail.com>
2020-03-30 23:39:12 +0200

    Unit to hold OXED UI basics

---
+++ oX/oxed/units/ui/oxeduUI.pas
@@ -0,0 +1,30 @@
+{
+   oxeduUI, oxed UI basics
+   Copyright (C) 2020. Dejan Boras
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxeduUI;
+
+INTERFACE
+
+   USES
+      uColors;
+
+TYPE
+
+   { oxedTUI }
+
+   oxedTUI = record
+      LazarusColor: TColor4ub;
+   end;
+
+VAR
+   oxedUI: oxedTUI;
+
+IMPLEMENTATION
+
+INITIALIZATION
+   oxedUI.LazarusColor.Assign(160, 160, 255, 255);
+
+END.
