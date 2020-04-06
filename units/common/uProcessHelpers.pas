d955c3b1aa1267bfd35842f041a88aee3702e0cc
Dejan Boras <de.jean.7777@protonmail.com>
2020-04-06 21:34:53 +0200

    Helper methods to get a list of process output strings

--- units/common/uProcessHelpers.pas
+++ units/common/uProcessHelpers.pas
@@ -9,7 +9,7 @@ UNIT uProcessHelpers;
 INTERFACE
 
    USES
-      process, pipes,
+      process, pipes, SysUtils, StreamIO,
       uStd, StringUtils;
 
 TYPE
@@ -17,27 +17,66 @@ TYPE
 
    TProcessUtils = record
       {get a input pipe stream as a string}
-      class function GetString(stream: TInputPipeStream; stripEndLine: boolean = true): string; static;
+      class function GetString(stream: TInputPipeStream; stripEndLine: boolean = true): StdString; static;
    end;
 
    { TProcessHelper }
 
    TProcessHelper = class helper for TProcess
-      function GetOutputString(stripEndLine: boolean = true): string;
+      function GetOutputString(stripEndLine: boolean = true): StdString;
+      function GetOutputStrings(stripEndLine: boolean = true; maxLines: loopint = -1): TSimpleStringList;
+      function GetOutputStrings(maxLines: loopint): TSimpleStringList;
    end;
-
 
 
 IMPLEMENTATION
 
 { TProcessHelper }
 
-function TProcessHelper.GetOutputString(stripEndLine: boolean): string;
+function TProcessHelper.GetOutputString(stripEndLine: boolean): StdString;
 begin
    Result := TProcessUtils.GetString(Output, stripEndLine);
 end;
 
-class function TProcessUtils.GetString(stream: TInputPipeStream; stripEndLine: boolean): string;
+function TProcessHelper.GetOutputStrings(stripEndLine: boolean; maxLines: loopint): TSimpleStringList;
+var
+   s: StdString;
+   f: TextFile;
+
+begin
+   TSimpleStringList.Initialize(Result);
+
+   {we do nothing}
+   if(maxLines = 0) then
+      exit;
+
+   ZeroOut(f, SizeOf(f));
+
+   AssignStream(f, Output);
+   Reset(f);
+
+   while(not eof(f)) do begin
+      ReadLn(f, s);
+
+      if(stripEndLine) then
+         StringUtils.stripEndLine(s);
+
+      Result.Add(s);
+
+      {check if we have the desired number of lines}
+      if(maxLines > 0) and (Result.n >= maxLines) then
+         break;
+   end;
+
+   Close(f);
+end;
+
+function TProcessHelper.GetOutputStrings(maxLines: loopint): TSimpleStringList;
+begin
+   Result := GetOutputStrings(true, maxLines);
+end;
+
+class function TProcessUtils.GetString(stream: TInputPipeStream; stripEndLine: boolean): StdString;
 var
    length: loopint;
 
