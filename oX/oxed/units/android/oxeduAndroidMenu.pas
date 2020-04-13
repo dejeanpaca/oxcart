84295bd49d818244a25a47a950bd4db3433d7f60
Dejan Boras <de.jean.7777@protonmail.com>
2020-04-13 23:03:30 +0200

    Give the android build icon some color

--- oX/oxed/units/android/oxeduAndroidMenu.pas
+++ oX/oxed/units/android/oxeduAndroidMenu.pas
@@ -9,6 +9,8 @@ UNIT oxeduAndroidMenu;
 INTERFACE
 
    USES
+      uColors,
+      {ui}
       wdguToolbar,
       {oxed}
       oxeduPlatform, oxeduAndroidPlatform, oxeduWorkbar,
@@ -26,8 +28,9 @@ begin
       item := oxedWorkbar.Workbar.FindItemByAction(oxedAndroidBuild.BUILD_TO_PROJECT_ACTION);
 
       if(item = nil) then begin
-         oxedWorkbar.Workbar.AddButton(oxedIcons.Create($f3cd), oxedAndroidBuild.BUILD_TO_PROJECT_ACTION)^.
-            Hint := 'Build to android project';
+         item := oxedWorkbar.Workbar.AddButton(oxedIcons.Create($f3cd), oxedAndroidBuild.BUILD_TO_PROJECT_ACTION);
+         item^.Hint := 'Build to android project';
+         item^.Color.Assign(61, 220, 132, 255);
       end;
    end;
 end;
