8265bd1d8d0c0e6fafe04af22a391538490c19f1
Dejan Boras <de.jean.7777@protonmail.com>
2020-05-02 22:02:58 +0200

    Unit for the oxTGlyph data type

---
+++ oX/units/oxuGlyph.pas
@@ -0,0 +1,39 @@
+{
+   oxuGlyph, glyph management
+   Copyright (c) 2020. Dejan Boras
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuGlyph;
+
+INTERFACE
+
+   USES
+      uStd,
+      {ox}
+      oxuTexture;
+
+TYPE
+   oxPGlyph = ^oxTGlyph;
+
+   { oxTGlyph }
+
+   oxTGlyph = record
+     BearingX,
+     BearingY,
+     Advance: loopint;
+     Texture: oxTTexture;
+
+     class procedure Init(out glyph: oxTGlyph); static;
+   end;
+
+IMPLEMENTATION
+
+{ oxTGlyph }
+
+class procedure oxTGlyph.Init(out glyph: oxTGlyph);
+begin
+   ZeroPtr(@glyph, SizeOf(glyph));
+end;
+
+END.
