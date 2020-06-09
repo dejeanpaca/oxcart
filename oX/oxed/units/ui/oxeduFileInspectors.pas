17a2889ab593e250b67dc575ed17f829e1363c06
Dejan Boras <de.jean.7777@protonmail.com>
2020-06-09 18:20:48 +0200

    Prepare some files for implementing a generic file inspector

---
+++ oX/oxed/units/ui/oxeduFileInspectors.pas
@@ -0,0 +1,13 @@
+{
+   oxeduFileInspectors, handles file inspectors
+   Copyright (C) 2020. Dejan Boras
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxeduFileInspectors;
+
+INTERFACE
+
+IMPLEMENTATION
+
+END.
