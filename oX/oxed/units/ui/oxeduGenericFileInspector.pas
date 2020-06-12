536ccb992ffa4817fddef4cab1392c44ea96d9f0
Dejan Boras <de.jean.7777@protonmail.com>
2020-06-13 00:04:16 +0200

    File inspectors can now be associated and opened

--- oX/oxed/units/ui/oxeduGenericFileInspector.pas
+++ oX/oxed/units/ui/oxeduGenericFileInspector.pas
@@ -9,8 +9,35 @@ UNIT oxeduGenericFileInspector;
 INTERFACE
 
    USES
+      uStd,
+      {oxed}
+      uOXED,
       oxeduInspectFile, oxeduFileInspectors;
 
+TYPE
+   oxedTGenericFileInspector = class(oxedTInspectFile)
+   end;
+
+VAR
+   oxedGenericFileInspector: oxedTGenericFileInspector;
+
 IMPLEMENTATION
+
+procedure init();
+begin
+   oxedGenericFileInspector := oxedTGenericFileInspector.Create();
+   oxedInspectFile.GenericInspector := oxedGenericFileInspector;
+end;
+
+procedure deinit();
+begin
+   if(oxedInspectFile.GenericInspector = oxedGenericFileInspector) then
+      oxedInspectFile.GenericInspector := nil;
+
+   FreeObject(oxedGenericFileInspector);
+end;
+
+INITIALIZATION
+   oxed.Init.Add('inspector.file.generic', @init, @deinit);
 
 END.
--- oX/oxed/units/ui/oxeduInspectFile.pas
+++ oX/oxed/units/ui/oxeduInspectFile.pas
@@ -9,7 +9,7 @@ UNIT oxeduInspectFile;
 INTERFACE
 
    USES
-      uStd,
+      uStd, StringUtils,
       {ui}
       uiWidgets,
       wdguLabel,
@@ -25,50 +25,116 @@ TYPE
 
       end;
 
-      procedure SetFile(const fn: StdString);
+      procedure SetFile(const {%H-}fn: StdString); virtual;
 
-      procedure Open(wnd: oxedTWindow); override;
-      procedure SizeChanged(wnd: oxedTWindow); override;
+      {associate this inspector with an extension}
+      procedure Associate(const extension: StdString);
+   end;
+
+   { oxedTFileInspectorAssociation }
+
+   oxedTFileInspectorAssociation = record
+      Extension: StdString;
+      Inspector: oxedTInspectFile;
+   end;
+
+   oxedTFileInspectorAssociations = specialize TSimpleList<oxedTFileInspectorAssociation>;
+
+   { oxedTInspectFileGlobal }
+
+   oxedTInspectFileGlobal = record
+      GenericInspector: oxedTInspectFile;
+      Inspectors: oxedTFileInspectorAssociations;
+
+      function FindInspectorByExtension(const ext: StdString): oxedTInspectFile;
+
+      procedure Open(const fn: StdString);
+      {associate an inspector with an extension}
+      procedure Associate(const extension: StdString; inspector: oxedTInspectFile);
    end;
 
 VAR
-   oxedInspectFile: oxedTInspectFile;
+   oxedInspectFile: oxedTInspectFileGlobal;
 
 IMPLEMENTATION
 
-{ oxedTInspectFile }
+{ oxedTInspectFileGlobal }
 
-procedure oxedTInspectFile.Open(wnd: oxedTWindow);
+function oxedTInspectFileGlobal.FindInspectorByExtension(const ext: StdString): oxedTInspectFile;
 var
-   inspector: oxedTInspectorWindow;
+   i: loopint;
+   {$IFDEF WINDOWS}
+   lext: StdString;
+   {$ENDIF}
 
 begin
-   inspector := oxedTInspectorWindow(wnd);
+   {$IFDEF WINDOWS}
+   lext := LowerCase(ext);
+   {$ENDIF}
+
+   for i := 0 to Inspectors.n - 1 do begin
+     {$IFDEF WINDOWS}
+     if(Inspectors.List[i].Extension = lext) then
+        exit(Inspectors.List[i].Inspector);
+     {$ELSE}
+     if(Inspectors.List[i].Extension = ext) then
+        exit(Inspectors.List[i].Inspector);
+     {$ENDIF}
+   end;
+
+   Result := nil;
 end;
+
+procedure oxedTInspectFileGlobal.Open(const fn: StdString);
+var
+   wnd: oxedTInspectorWindow;
+   inspector: oxedTInspectFile;
+
+begin
+   if(not oxedInspector.GetWindow(wnd)) then
+      exit;
+
+   if(fn <> '') then begin
+      {find inspector}
+      inspector := FindInspectorByExtension(ExtractFileExt(fn));
+
+      {no inspector for file found, so use global one if one is set}
+      if(inspector = nil) then
+         inspector := GenericInspector;
+
+      if(inspector <> nil) then begin
+         writeln('got inspector: ', inspector.ClassName);
+         wnd.Open(inspector);
+         inspector.SetFile(fn);
+      end else
+         wnd.Open(nil);
+   end else
+      wnd.Open(nil);
+end;
+
+procedure oxedTInspectFileGlobal.Associate(const extension: StdString; inspector: oxedTInspectFile);
+var
+   association: oxedTFileInspectorAssociation;
+
+begin
+   association.Extension := extension;
+   association.Inspector := inspector;
+
+   Inspectors.Add(association);
+end;
+
+{ oxedTInspectFile }
 
 procedure oxedTInspectFile.SetFile(const fn: StdString);
 begin
 end;
 
-procedure oxedTInspectFile.SizeChanged(wnd: oxedTWindow);
-var
-   inspector: oxedTInspectorWindow;
-
+procedure oxedTInspectFile.Associate(const extension: StdString);
 begin
-   inspector := oxedTInspectorWindow(wnd);
-end;
-
-procedure init();
-begin
-   oxedInspectFile := oxedTInspectFile.Create();
-end;
-
-procedure deinit();
-begin
-   FreeObject(oxedInspectFile);
+   oxedInspectFile.Associate(extension, Self);
 end;
 
 INITIALIZATION
-   oxed.Init.Add('inspector.file', @init, @deinit);
+   oxedTFileInspectorAssociations.Initialize(oxedInspectFile.Inspectors, 256);
 
 END.
