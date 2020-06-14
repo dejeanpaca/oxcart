8d2f6106fc70f96fa566398a54cbfa8142d765b5
Dejan Boras <de.jean.7777@protonmail.com>
2020-06-14 23:37:45 +0200

    Start implementing some basic general file inspector UI

--- oX/oxed/units/ui/oxeduGenericFileInspector.pas
+++ oX/oxed/units/ui/oxeduGenericFileInspector.pas
@@ -10,12 +10,26 @@ INTERFACE
 
    USES
       uStd,
+      {ui}
+      uiWidgets, uiuWidget,
+      wdguWorkbar, wdguGroup, wdguInputBox,
       {oxed}
-      uOXED,
-      oxeduInspectFile, oxeduFileInspectors;
+      uOXED, oxeduWindow, oxeduwndInspector,
+      oxeduInspectFile;
 
 TYPE
+
+   { oxedTGenericFileInspector }
+
    oxedTGenericFileInspector = class(oxedTInspectFile)
+      wdg: record
+         Information: wdgTGroup;
+         Name: wdgTInputBox;
+      end;
+
+      procedure SetFile(const fn: StdString); override;
+      procedure Open(wnd: oxedTWindow); override;
+      procedure SizeChanged(wnd: oxedTWindow); override;
    end;
 
 VAR
@@ -35,6 +49,69 @@ begin
       oxedInspectFile.GenericInspector := nil;
 
    FreeObject(oxedGenericFileInspector);
+end;
+
+{ oxedTGenericFileInspector }
+
+procedure oxedTGenericFileInspector.SetFile(const fn: StdString);
+var
+   inspector: oxedTInspectorWindow;
+
+begin
+   inspector := oxedTInspectorWindow(oxedInspector.Instance);
+
+   if(fn <> '') then begin
+      wdg.Name.Enable(true);
+      wdg.Name.SetText(fn);
+   end else begin
+      wdg.Name.Enable(false);
+   end;
+
+   SizeChanged(inspector);
+end;
+
+procedure oxedTGenericFileInspector.Open(wnd: oxedTWindow);
+var
+   inspector: oxedTInspectorWindow;
+   group: oxedTInspectorWindowGroup;
+
+begin
+   inspector := oxedTInspectorWindow(wnd);
+
+   { header }
+   inspector.wdg.Header := wdgWorkbar.Add(oxedInspector.Instance);
+
+   uiWidget.PushTarget();
+   inspector.wdg.Header.SetTarget();
+
+   wdg.Name := wdgInputBox.Add('');
+   wdg.Name.ReadOnly := true;
+
+   uiWidget.PopTarget();
+
+   { transform group }
+   group := inspector.AddGroup('Information');
+
+   wdg.Information := group.Wdg;
+   uiWidget.PushTarget();
+
+   uiWidget.PopTarget();
+
+   SetFile('');
+end;
+
+procedure oxedTGenericFileInspector.SizeChanged(wnd: oxedTWindow);
+var
+   inspector: oxedTInspectorWindow;
+
+begin
+   inspector := oxedTInspectorWindow(wnd);
+   inspector.wdg.Header.Move(0, wnd.Dimensions.h - 1);
+   inspector.wdg.Header.Resize(wnd.Dimensions.w, 32);
+
+   wdg.Name.Resize(wnd.Dimensions.w - wdgDEFAULT_SPACING * 2, inspector.wdg.Header.Dimensions.h - 10);
+   wdg.Name.Move(wdg.Name.Position.x, 20);
+   wdg.Name.SetPosition(wdgPOSITION_VERTICAL_CENTER);
 end;
 
 INITIALIZATION
