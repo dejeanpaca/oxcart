d918e7546b0b4c63166c9447c35dd34733ddbdcf
Dejan Boras <de.jean.7777@protonmail.com>
2020-06-24 11:27:24 +0200

    Prepare unit for skybox component

---
+++ oX/units/components/oxuSkyboxComponent.pas
@@ -0,0 +1,75 @@
+{
+   oxuSkyboxComponent, sky box
+   Copyright (c) 2020. Dejan Boras
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuSkyboxComponent;
+
+INTERFACE
+
+   USES
+      uStd,
+      {ox}
+      oxuComponent, oxuComponentDescriptors, oxuRenderComponent, oxuEntity,
+      oxuSerialization;
+
+TYPE
+
+   { oxTSkyboxComponent }
+
+   oxTSkyboxComponent = class(oxTRenderComponent)
+      public
+      constructor Create(); override;
+
+      procedure Render(); override;
+
+      function GetDescriptor(): oxPComponentDescriptor; override;
+
+      class function GetEntity(out component: oxTSkyboxComponent): oxTEntity; static;
+   end;
+
+IMPLEMENTATION
+
+VAR
+   serializer: oxTSerialization;
+   descriptor: oxTComponentDescriptor;
+
+{ oxTSkyboxComponent }
+
+constructor oxTSkyboxComponent.Create();
+begin
+   inherited;
+end;
+
+procedure oxTSkyboxComponent.Render();
+begin
+   {TODO: Render Skybox}
+end;
+
+function oxTSkyboxComponent.GetDescriptor(): oxPComponentDescriptor;
+begin
+   Result := @descriptor;
+end;
+
+class function oxTSkyboxComponent.GetEntity(out component: oxTSkyboxComponent): oxTEntity;
+begin
+   component := oxTSkyboxComponent.Create();
+   Result := oxEntity.New('Skybox', component);
+end;
+
+function instance(): TObject;
+begin
+   Result := oxTSkyboxComponent.Create();
+end;
+
+INITIALIZATION
+   serializer := oxTSerialization.Create(oxTSkyboxComponent, @instance);
+
+   descriptor.Create('skybox', oxTSkyboxComponent);
+   descriptor.Name := 'Skybox';
+
+FINALIZATION
+   FreeObject(serializer);
+
+END.
