69c1b8d8db6fbace8104e15f0b1835101525925d
Dejan Boras <de.jean.7777@protonmail.com>
2020-06-26 22:22:46 +0200

    Improvements and fixes for 9p file reader

--- oX/units/oxu9PatchFileReader.pas
+++ oX/units/oxu9PatchFileReader.pas
@@ -12,7 +12,7 @@ INTERFACE
       sysutils,
       uStd, uFile, uFileHandlers, uLog, StringUtils,
       {ox}
-      oxuTypes,
+      oxuTypes, oxuTexture, oxuTextureGenerate,
       oxuFile, oxu9Patch, oxu9PatchFile;
 
 IMPLEMENTATION
@@ -37,6 +37,9 @@ var
    rows: array[0..2] of TPatchRow;
 
    patch: oxT9Patch;
+   texture: oxTTexture;
+
+   p: loopint;
 
    {break down a line into its components}
    function breakdown(): boolean;
@@ -50,55 +53,84 @@ var
          exit(false);
 
       {get the left component}
-      wd := CopyToDel(s);
+      wd := CopyToDel(s, '-');
+      StripWhitespace(wd);
 
       if(not row.left.FromString(wd)) then
          exit(false);
 
       {get the middle component (it's a single number, not WxD)}
-      wd := CopyToDel(s);
+      wd := CopyToDel(s, '-');
+      StripWhitespace(wd);
 
       if(not loopint.TryParse(wd, row.middle.w)) then
          exit(false);
 
       row.middle.h := row.middle.w;
 
-      {get the right component}
-      wd := CopyToDel(s);
+      {get the right component (what is left of the current line)}
+      StripWhitespace(s);
 
-      if(not row.right.FromString(wd)) then
+      if(not row.right.FromString(s)) then
          exit(false);
 
       Result := true;
    end;
 
 begin
+   texture := nil;
+
    {top}
-   if(not breakdown()) then
+   if(not breakdown()) then begin
+      data.SetError(eINVALID, 'Failed to process top row');
       exit;
+   end;
 
    rows[0] := row;
 
    {middle}
-   if(not breakdown()) then
+   if(not breakdown()) then begin
+      data.SetError(eINVALID, 'Failed to process middle row');
       exit;
+   end;
 
    rows[1] := row;
 
    {bottom}
-   if(not breakdown()) then
+   if(not breakdown()) then begin
+      data.SetError(eINVALID, 'Failed to process bottom row');
       exit;
+   end;
 
    rows[2] := row;
+
+   repeat
+      f.Readln(s);
+      p := pos('texture: ', s);
+
+      if(p > 0) then begin
+         {we have a texture}
+         s := Copy(s, Length('texture: ') + 1, Length('texture: ') - p + 2);
+
+         oxTextureGenerate.Generate(ExtractFilePath(f.fn) + s, texture);
+
+         break;
+      end;
+   until f.EOF() or (f.Error <> 0);
+
+   if(f.Error <> 0) then
+      exit;
 
    {TODO: Do some validation}
 
    patch := oxT9Patch.Create();
 
+   patch.Texture := texture;
+
    {get sizes}
 
-   patch.Sizes.Width := rows[0].left.w + rows[0].middle.w + rows[0].right.w;
-   patch.Sizes.Height := rows[0].left.h + rows[1].left.h + rows[2].left.h;
+   patch.Sizes.Width  := rows[0].left.w + rows[0].middle.w + rows[0].right.w;
+   patch.Sizes.Height := rows[0].left.h + rows[1].middle.h + rows[2].right.h;
 
    {top}
    patch.Sizes.TopLeft[0]  := rows[0].left.w;
@@ -129,6 +161,8 @@ begin
 
    patch.Sizes.BottomRight[0] := rows[2].right.w;
    patch.Sizes.BottomRight[1] := rows[2].right.h;
+
+   patch.Build();
 
    data.Result := patch;
 end;
