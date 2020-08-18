afee756dddf80a6e993c2b07bdbe5aa5dffd1fc0
Dejan Boras <de.jean.7777@protonmail.com>
2020-08-18 23:03:03 +0200

    Rendering context should have the RC Id associated

--- oX/units/oxuRenderingContext.pas
+++ oX/units/oxuRenderingContext.pas
@@ -18,10 +18,13 @@ TYPE
 
    oxTRenderingContext = record
       Name: StdString;
+      {rendering context Id of the renderer}
+      RC: loopint;
 
       Viewport: oxPViewport;
       Projection: oxPProjection;
       Camera: pointer;
+      {window associated with this rendering context, if any}
       Window: oxTWindow;
    end;
 
