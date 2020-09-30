042c32e546cbad518f0209ce29cb53169a373608
Dejan Boras <de.jean.7777@protonmail.com>
2020-09-30 18:02:18 +0200

    Separate error handler into its own unit

---
+++ units/common/uErrorCrashHandler.pas
@@ -0,0 +1,64 @@
+{
+   uError
+   Copyright (C) Dejan Boras 2020.
+}
+
+{$INCLUDE oxheader.inc}
+UNIT uErrorCrashHandler;
+
+INTERFACE
+
+   USES
+      sysutils, uStd, uError;
+
+IMPLEMENTATION
+
+VAR
+   oldExitProc: pointer;
+
+procedure RunTimeErrorDisplay(addr: pointer);
+var
+   s: StdString;
+
+begin
+   {display the error message}
+   if(addr <> nil) and (isConsole) then begin
+      writeln(stdout, '┻━┻ ︵ ╯(°□° ╯)');
+
+      s := getRunTimeErrorDescription(ErrorCode);
+
+      writeln(stdout, 'Error (', ErrorCode, '): ', s, ' @ $', addr2str(addr));
+   end;
+end;
+
+procedure RunTimeError();
+begin
+   {restore the previous error handler}
+   ExitProc := oldExitProc;
+
+   RunTimeErrorDisplay(ErrorAddr);
+end;
+
+procedure UnhandledException(obj: TObject; addr: Pointer; {%H-}frameCount: Longint; {%H-}frames: PPointer);
+begin
+   writeln(stdout, '(╯°□°)╯︵ ┻━┻');
+   writeln(stdout, 'Unhandled exception @ $',  addr2str(addr), ' :');
+
+   if(obj is Exception) then begin
+      writeln(stdout, DumpExceptionCallStack(Exception(obj)));
+   end else begin
+      writeln(stdout, 'Exception object ', obj.ClassName, ' is not of class Exception.');
+      writeln(stdout, DumpExceptionCallStack(addr, frameCount, frames));
+   end;
+
+   writeln(stdout,'');
+end;
+
+INITIALIZATION
+   {store the old exit proc and set the new one}
+   oldExitProc := ExitProc;
+   ExitProc := @RunTimeError;
+
+   ExceptProc := @UnhandledException;
+
+END.
