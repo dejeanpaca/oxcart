af88d4dc42878049ea1550334be721bd1d564ced
Dejan Boras <de.jean.7777@protonmail.com>
2020-11-30 23:17:50 +0100

    Improve xbox controller mapping

--- components/app/units/controllers/appuControllerMappings.pas
+++ components/app/units/controllers/appuControllerMappings.pas
@@ -23,28 +23,39 @@ procedure initialize360(out m: appTControllerDeviceMapping);
 begin
    appTControllerDeviceMapping.Initialize(m);
 
-   {$IFDEF WINDOWS}
-   m.Settings.ButtonCount := 16;
-   {$ENDIF}
-   {$IFDEF LINUX}
-   m.Settings.ButtonCount := 10;
-   {$ENDIF}
-
    m.Settings.AxisCount := 4;
    m.Settings.TriggerCount := 2;
    m.Settings.AxisGroupCount := 2;
    m.Settings.DPadPresent := true;
 
+   {$IFDEF WINDOWS}
+   m.Settings.ButtonCount := 16;
+   {$ENDIF}
+   {$IFDEF LINUX}
+   m.Settings.ButtonCount := 10;
+   m.Settings.RemappedAxisCount := 8;
+
+   m.Settings.AxisGroups[0][0] := 0;
+   m.Settings.AxisGroups[0][1] := 1;
+
+   m.Settings.AxisGroups[1][0] := 2;
+   m.Settings.AxisGroups[1][1] := 3;
+   {$ENDIF}
+
    m.Id := 'xbox360';
    m.RecognitionString := '360';
 
    {$IFDEF LINUX}
+   {left thumbstick}
+   m.AxisRemaps[1].RemapType := appCONTROLLER_AXIS_IS_INVERTED;
+
    {left trigger}
    m.AxisRemaps[2].RemapType := appCONTROLLER_AXIS_IS_TRIGGER;
    m.AxisRemaps[2].Index := 0;
 
    {right thumbstick}
    m.AxisRemaps[3].Index := 2;
+   m.AxisRemaps[4].RemapType := appCONTROLLER_AXIS_IS_INVERTED;
    m.AxisRemaps[4].Index := 3;
 
    {right trigger}
