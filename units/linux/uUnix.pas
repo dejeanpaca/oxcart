c652c99e6cf272726b57c32aecfc9c9bb74a7ffb
Dejan Boras <de.jean.7777@protonmail.com>
2020-12-04 00:14:26 +0100

    Implement unxFpPipe() helper method

--- units/linux/uUnix.pas
+++ units/linux/uUnix.pas
@@ -9,11 +9,26 @@ UNIT uUnix;
 INTERFACE
 
    USES
-      unix;
+      unix, baseunix;
+
+TYPE
+   {a read/write fd pipe descriptor}
+   unxTPipe = array[0..1] of cint;
 
 function unxFpread(fd: cint; buf: pchar; nbytes: TSize): TSsize; external name 'FPC_SYSC_READ';
 function unxFpWrite(fd : cInt; buf: pChar; nbytes: TSize): TSsize;  external name 'FPC_SYSC_WRITE';
 
+{helper method to more easily initialize a pipe (via FpPipe)}
+function unxFpPipe(out pipe: unxTPipe): cint;
+
 IMPLEMENTATION
+
+function unxFpPipe(out pipe: unxTPipe): cint;
+begin
+   pipe[0] := 0;
+   pipe[1] := 0;
+
+   Result := FpPipe(pipe);
+end;
 
 END.
