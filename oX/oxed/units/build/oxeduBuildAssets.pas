9c2c4a02fda5dcab3be9f6f20428dcd238b9cb5b
Dejan Boras <de.jean.7777@protonmail.com>
2020-12-08 23:10:37 +0100

    Base oxedTBuildAssets on oxedTProjectWalker

--- oX/oxed/units/build/oxeduBuildAssets.pas
+++ oX/oxed/units/build/oxeduBuildAssets.pas
@@ -9,7 +9,7 @@ UNIT oxeduBuildAssets;
 INTERFACE
 
    USES
-      sysutils, uStd, uError, uLog, uFileUtils, StringUtils,
+      sysutils, uStd, uLog, uFileUtils, StringUtils,
       {oxed}
       uOXED, oxeduBuildLog,
       oxeduPackage, oxeduPackageTypes,
@@ -42,13 +42,8 @@ TYPE
 
    { oxedTBuildAssets }
 
-   oxedTBuildAssets = record
-      Walker: TFileTraverse;
-
+   oxedTBuildAssets = class(oxedTProjectWalker)
       FileCount: loopint;
-
-
-      Current: oxedTProjectWalkerCurrent;
 
       {target path where assets will go}
       Target,
@@ -59,19 +54,21 @@ TYPE
 
       {called before assets are deployed (hook your deployer here)}
       PreDeploy: TProcedures;
-      {called when a file is handled}
-      OnFile: oxedTProjectWalkerFileProcedures;
 
       {currently used assets deployer}
       Deployer,
       {default deployer set on every build}
       DefaultDeployer: oxedTAssetsDeployer;
 
-      procedure Initialize();
+      constructor Create(); override;
+
       {deploys asset files to the given target}
       procedure Deploy(const useTarget: StdString);
-      {deploy the specified package}
-      procedure DeployPackage(var p: oxedTPackage);
+
+      protected
+         function HandleFile(var f: oxedTProjectWalkerFile; const fd: TFileTraverseData): boolean; override;
+         function HandleDirectory(var dir: StdString; const {%H-}fd: TFileTraverseData): boolean; override;
+         function HandlePackage(var package: oxedTPackage): boolean; override;
    end;
 
 VAR
@@ -96,37 +93,47 @@ begin
    Result := true;
 end;
 
-function scanFile(const fd: TFileTraverseData): boolean;
-var
-   f: oxedTProjectWalkerFile;
-   aF: oxedTAssetBuildFile;
+{ oxedTBuildAssets }
 
-   source,
-   target: StdString;
+constructor oxedTBuildAssets.Create();
+begin
+   inherited;
+
+   HandleOx := false;
+end;
+
+procedure oxedTBuildAssets.Deploy(const useTarget: StdString);
+begin
+   Target := IncludeTrailingPathDelimiter(useTarget);
+   oxedBuildLog.i('Deploying asset files to ' + useTarget);
+   Deployer := DefaultDeployer;
+   FileCount := 0;
+
+   PreDeploy.Call();
+
+   Run();
+
+   oxedBuildLog.i('Done assets deploy (files: ' + sf(FileCount) + ')');
+end;
+
+function oxedTBuildAssets.HandleFile(var f: oxedTProjectWalkerFile; const fd: TFileTraverseData): boolean;
+var
+   aF: oxedTAssetBuildFile;
 
 begin
    Result := true;
 
-   {ignore stuff in the temp directory}
-   if(Pos(oxPROJECT_TEMP_DIRECTORY, fd.f.Name) = 1) then
-      exit;
-
-   oxedBuildAssets.Current.FormFile(f, fd.f);
-
    if(oxedAssets.ShouldIgnore(f.Extension)) then begin
       consoleLog.v('Ignoring: ' + fd.f.Name);
-      exit;
+      exit(True);
    end;
 
    oxedBuildLog.v('Deploying: ' + fd.f.Name);
 
-   source := f.PackagePath + f.PackageFileName;
-   target := oxedBuildAssets.CurrentTarget + f.PackageFileName;
-
    ZeroOut(aF, SizeOf(aF));
 
-   aF.Source := source;
-   aF.Target := target;
+   aF.Source := f.PackagePath + f.PackageFileName;
+   aF.Target := oxedBuildAssets.CurrentTarget + f.PackageFileName;
 
    aF.CurrentPackage := oxedBuildAssets.Current.Package;
 
@@ -134,105 +141,50 @@ begin
       exit(False);
 
    inc(oxedBuildAssets.FileCount);
-
-   oxedBuildAssets.OnFile.Call(f);
 end;
 
-function onDirectory(const fd: TFileTraverseData): boolean;
+function oxedTBuildAssets.HandleDirectory(var dir: StdString; const fd: TFileTraverseData): boolean;
 var
-   dir: StdString;
    pp: oxedPPackagePath;
 
 begin
    Result := true;
 
-   dir := oxedTProjectWalker.GetValidPath(oxedBuildAssets.Current.Path, fd.f.Name);
+   {find closest package path, and skip if optional}
+   pp := oxedBuildAssets.Current.Package^.Paths.FindClosest(dir);
 
-   if(dir <> '') then begin
-      {Find closest package path, and skip if optional}
-      pp := oxedBuildAssets.Current.Package^.Paths.FindClosest(dir);
-
-      if(pp <> nil) and (pp^.IsOptional()) then begin
-         oxedBuildLog.v('Optional: ' + dir);
-         exit(False);
-      end;
-   end else
-      Result := false;
-end;
-
-{ oxedTBuildAssets }
-
-procedure oxedTBuildAssets.Initialize();
-begin
-   TFileTraverse.Initialize(Walker);
-
-   Walker.OnFile := @scanFile;
-   Walker.OnDirectory := @onDirectory;
-
-   OnFile.Initialize(OnFile);
-end;
-
-procedure oxedTBuildAssets.Deploy(const useTarget: StdString);
-var
-   i: loopint;
-
-begin
-   Target := IncludeTrailingPathDelimiter(useTarget);
-   oxedBuildLog.i('Deploying asset files to ' + useTarget);
-
-   Deployer := DefaultDeployer;
-
-   FileCount := 0;
-
-   PreDeploy.Call();
-
-   try
-      {deploy assets from ox, but only those in the data directory}
-      TargetSuffix := 'data';
-
-      DeployPackage(oxedAssets.oxDataPackage);
-
-      {deploy assets from packages}
-      for i := 0 to oxedProject.Packages.n - 1 do begin
-         DeployPackage(oxedProject.Packages.List[i]);
-      end;
-
-      {deploy assets from project}
-      DeployPackage(oxedProject.MainPackage);
-   except
-      on e: Exception do begin
-         oxedBuildLog.e('Asset deployment failed running');
-         oxedBuildLog.e(DumpExceptionCallStack(e));
-      end;
+   if(pp <> nil) and (pp^.IsOptional()) then begin
+      oxedBuildLog.v('Optional: ' + dir);
+      exit(False);
    end;
-
-   Current.Package := nil;
-
-   oxedBuildLog.i('Done assets deploy (files: ' + sf(FileCount) + ')');
 end;
 
-procedure oxedTBuildAssets.DeployPackage(var p: oxedTPackage);
+function oxedTBuildAssets.HandlePackage(var package: oxedTPackage): boolean;
 begin
-   Current.Package := @p;
-   Current.Path := oxedProject.GetPackagePath(p);
-
-   if(TargetSuffix = '') then
-      CurrentTarget := Target
-   else
-      CurrentTarget := IncludeTrailingPathDelimiter(Target + TargetSuffix);
+   Result := true;
 
    oxedBuildLog.v('Deploying package: ' + Current.Path);
-   Walker.Run(oxedBuildAssets.Current.Path);
 
-   TargetSuffix := '';
+   if(@package = @oxedAssets.oxDataPackage) then begin
+      CurrentTarget := IncludeTrailingPathDelimiter(Target + TargetSuffix);
+      TargetSuffix := 'data';
+   end else begin
+      TargetSuffix := '';
+      CurrentTarget := Target;
+   end;
 end;
 
-procedure init();
+procedure initialize();
 begin
-   oxedBuildAssets.Initialize();
+   oxedBuildAssets := oxedTBuildAssets.Create();
+end;
+
+procedure deinitialize();
+begin
+   FreeObject(oxedBuildAssets);
 end;
 
 INITIALIZATION
-   oxed.Init.Add('build_assets', @init);
+   oxed.Init.Add('build_assets', @initialize, @deinitialize);
 
 END.
