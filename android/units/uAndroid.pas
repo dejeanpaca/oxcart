e633d6eea51de308b34b0d9506831a326caf387f
Dejan Boras <de.jean.7777@protonmail.com>
2020-12-21 11:27:14 +0100

    Maybe we just use what's provided already

--- android/units/uAndroid.pas
+++ android/units/uAndroid.pas
@@ -56,12 +56,9 @@ CONST
 VAR
    android_api_level: longint;
 
-function android_get_application_target_sdk_version(): cint; cdecl; external;
-function android_get_device_api_level(): cint; cdecl; external;
-
 IMPLEMENTATION
 
 INITIALIZATION
-   android_api_level :=  android_get_device_api_level();
+   android_api_level := SystemApiLevel();
 
 END.
