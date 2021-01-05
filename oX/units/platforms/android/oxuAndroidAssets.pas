f24f1f33eaf8c7f6e76aa2fc1d0ba2ce3ba55914
Dejan Boras <de.jean.7777@protonmail.com>
2021-01-05 22:52:43 +0100

    Try to load android asset data file

---
+++ oX/units/platforms/android/oxuAndroidAssets.pas
@@ -0,0 +1,53 @@
+{
+   oxuAndroidAssets
+   Copyright (c) 2020. Dejan Boras
+}
+
+{$INCLUDE oxheader.inc}
+UNIT oxuAndroidAssets;
+
+INTERFACE
+
+   USES
+      uStd, uFile,
+      {android}
+      uAndroidAssets, android_log_helper;
+
+TYPE
+
+   { oxTAndroidAssets }
+
+   oxTAndroidAssets = record
+      fData: TFile;
+
+      procedure Initialize();
+      procedure DeInitialize();
+   end;
+
+VAR
+   oxAndroidAssets: oxTAndroidAssets;
+
+IMPLEMENTATION
+
+{ oxTAndroidAssets }
+
+procedure oxTAndroidAssets.Initialize();
+var
+   fn: StdString;
+
+begin
+   fn := 'assets/data.ypk';
+
+   if(androidAssetManager.Open(fn, fData)) then
+      logi('Opened assets file')
+   else
+      loge('Failed to open assets file: ' + fn);
+end;
+
+procedure oxTAndroidAssets.DeInitialize();
+begin
+   if(fData.IsOpened()) then
+      fData.Close();
+end;
+
+END.
