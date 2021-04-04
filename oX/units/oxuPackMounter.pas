6eb1f8c148b523b0800ed25c8679747064467bda
Dejan Boras <de.jean.7777@protonmail.com>
2021-04-04 23:34:09 +0200

    oxuPackMounter now mounts the default data.ypk file

--- oX/units/oxuPackMounter.pas
+++ oX/units/oxuPackMounter.pas
@@ -9,13 +9,36 @@ UNIT oxuPackMounter;
 INTERFACE
 
 USES
-   ypkuFS
+   uStd, ypkuFS, uLog,
+   {ox}
+   uOX, oxuPaths
    {$IFDEF ANDROID}
    , oxuAndroidAssets
    {$ENDIF};
 
 IMPLEMENTATION
 
+procedure init();
+var
+   base,
+     path: StdString;
+
+begin
+   base := 'data.ypk';
+   path := oxPaths.Find(base);
+
+   if(path <> '') then
+      ypkfs.Add(path)
+   else
+      log.e('Could not find ' + base + ' pack file');
+end;
+
+procedure deinit();
+begin
+   ypkfs.Unmount();
+end;
+
 INITIALIZATION
+   ox.PreInit.Add('pack_mounter', @init, @deinit);
 
 END.
