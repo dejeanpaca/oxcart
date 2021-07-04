10a4a7da4da27bcf606efa3d8dc40e28a704a210
Dejan Boras <de.jean.7777@protonmail.com>
2021-07-04 14:00:11 +0200

    Surface renderer can accept a context and method to render, instead of using surface renderers

--- oX/units/oxuSurfaceRender.pas
+++ oX/units/oxuSurfaceRender.pas
@@ -17,6 +17,7 @@ USES
 
 TYPE
    oxTSurfaceRenderRoutine = procedure(var context: oxTRenderingContext);
+   oxTSurfaceRenderObjectRoutine = procedure(var context: oxTRenderingContext) of object;
 
    oxPSurfaceRenderer = ^oxTSurfaceRenderer;
 
@@ -27,6 +28,15 @@ TYPE
    end;
 
    oxTSurfaceRenderers = specialize TSimpleList<oxPSurfaceRenderer>;
+
+   oxTSurfaceRenderContext = record
+      Window: oxTWindow;
+
+      {routine used to render}
+      Routine: oxTSurfaceRenderRoutine;
+      {object routine used to render}
+      ObjectRoutine: oxTSurfaceRenderObjectRoutine;
+   end;
 
    { oxTSurfaceRenderGlobal }
 
@@ -39,7 +49,14 @@ TYPE
       {get a surface renderer entry and add it to the list}
       procedure Get(out renderer: oxTSurfaceRenderer; routine: oxTSurfaceRenderRoutine);
 
+      class procedure Initialize(out context: oxTSurfaceRenderContext); static;
+
+      {render a surface context}
+      procedure Render(var context: oxTSurfaceRenderContext);
+      {render a window}
       procedure Render(wnd: oxTWindow);
+      {render window with this method only}
+      procedure RenderOnly(wnd: oxTWindow; renderRoutine: oxTSurfaceRenderObjectRoutine);
    end;
 
 VAR
@@ -69,12 +86,20 @@ begin
    List.Add(@renderer);
 end;
 
-procedure oxTSurfaceRenderGlobal.Render(wnd: oxTWindow);
+class procedure oxTSurfaceRenderGlobal.Initialize(out context: oxTSurfaceRenderContext);
+begin
+   ZeroOut(context, SizeOf(context));
+end;
+
+procedure oxTSurfaceRenderGlobal.Render(var context: oxTSurfaceRenderContext);
 var
    rc: oxPRenderingContext;
    i: loopint;
+   wnd: oxTWindow;
 
 begin
+   wnd := context.Window;
+
    if(wnd.oxProperties.ApplyDefaultViewport) then
       wnd.Viewport.Apply();
 
@@ -86,14 +111,43 @@ begin
    rc^.Name := wnd.Title;
    rc^.RC := wnd.RenderingContext;
 
-   for i := 0 to List.n - 1 do begin
-      List.List[i]^.Routine(rc^);
+   if(context.Routine = nil) and (context.ObjectRoutine = nil) then begin
+      for i := 0 to List.n - 1 do begin
+         List.List[i]^.Routine(rc^);
+      end;
+   end else begin
+      if(context.Routine <> nil) then
+         context.Routine(rc^);
+
+      if(context.ObjectRoutine <> nil) then
+         context.ObjectRoutine(rc^);
    end;
 
    if(not ox.LibraryMode) then
       oxTRenderer(wnd.Renderer).SwapBuffers(wnd);
 
    wnd.Viewport.Done();
+end;
+
+procedure oxTSurfaceRenderGlobal.Render(wnd: oxTWindow);
+var
+   context: oxTSurfaceRenderContext;
+
+begin
+   Initialize(context);
+   context.Window := wnd;
+   Render(context);
+end;
+
+procedure oxTSurfaceRenderGlobal.RenderOnly(wnd: oxTWindow; renderRoutine: oxTSurfaceRenderObjectRoutine);
+var
+   context: oxTSurfaceRenderContext;
+
+begin
+   Initialize(context);
+   context.Window := wnd;
+   context.ObjectRoutine := renderRoutine;
+   Render(context);
 end;
 
 INITIALIZATION
