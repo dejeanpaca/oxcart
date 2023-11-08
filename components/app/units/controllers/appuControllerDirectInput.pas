725568b0b8f9a0837b2d5550db458cd31a1c3d18
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:28:24 +0100

    We'll also support DirectInput devices too (eventually)

---
+++ components/app/units/controllers/appuControllerDirectInput.pas
@@ -0,0 +1,84 @@
+{
+   appuControllerDirectInput, DirectInput controller handling
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    19.09.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT appuControllerDirectInput;
+
+INTERFACE
+
+   USES
+      uStd, uLog,
+      appuController;
+
+TYPE
+
+   { appTDirectInputControllerHandler }
+
+   appTDirectInputControllerHandler = class(appTControllerHandler)
+      procedure Initialize(); override;
+      procedure Reset(); override;
+      procedure Run(); override;
+
+      private
+         procedure Add(const fn: string);
+   end;
+
+   { appTDirectInputControllerDevice }
+
+   appTDirectInputControllerDevice = class(appTControllerDevice)
+      FileName: string;
+
+      procedure Run(); override;
+      procedure DeInitialize(); override;
+   end;
+
+IMPLEMENTATION
+
+{ appTDirectInputControllerDevice }
+
+procedure appTDirectInputControllerDevice.Run();
+begin
+end;
+
+procedure appTDirectInputControllerDevice.DeInitialize();
+begin
+   inherited;
+end;
+
+{ appTDirectInputControllerHandler }
+
+procedure appTDirectInputControllerHandler.Initialize();
+begin
+   Reset();
+end;
+
+procedure appTDirectInputControllerHandler.Reset();
+begin
+   inherited;
+end;
+
+procedure appTDirectInputControllerHandler.Run();
+begin
+   inherited Run();
+end;
+
+procedure appTDirectInputControllerHandler.Add(const fn: string);
+var
+   device: appTDirectInputControllerDevice;
+
+begin
+   device := appTDirectInputControllerDevice.Create();
+
+   // device.Initialize(fn);
+
+   appControllers.Add(device);
+end;
+
+INITIALIZATION
+   appControllerHandler := appTDirectInputControllerHandler.Create();
+
+END.
