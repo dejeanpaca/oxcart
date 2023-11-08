d815f6bbd494aadeeecadfe68b74cd4a6623b138
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:58:28 +0100

    Unit with unix helpers

---
+++ units/linux/uUnix.pas
@@ -0,0 +1,19 @@
+{
+   uUnix
+   Copyright (C) 2020. Dejan Boras
+}
+
+{$INCLUDE oxheader.inc}
+UNIT uUnix;
+
+INTERFACE
+
+   USES
+      ctypes, unix;
+
+function unxFpread(fd: cint; buf: pchar; nbytes: TSize): TSsize; external name 'FPC_SYSC_READ';
+function unxFpWrite(fd : cInt; buf: pChar; nbytes: TSize): TSsize;  external name 'FPC_SYSC_WRITE';
+
+IMPLEMENTATION
+
+END.
