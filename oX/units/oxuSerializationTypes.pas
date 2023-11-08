3a6c081434ff0f6f4f929ed61740b837f3040a2c
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:30:11 +0100

    Improvements to serialization
    - A record type for quick record types (records not holding any other managed types)
    - Unit for common oX serializable types

---
+++ oX/units/oxuSerializationTypes.pas
@@ -0,0 +1,47 @@
+{
+   oxuTypes, common oX data types
+   Copyright (c) 2011. Dejan Boras
+
+   Started On:    09.02.2011.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuSerializationTypes;
+
+INTERFACE
+
+   USES
+      oxuTypes, oxuSerialization;
+
+TYPE
+   oxTSerializationTypes = record
+     tPoint,
+     tDimensions,
+     tPointf,
+     tDimensionsf: oxTSerializationDataType;
+   end;
+
+VAR
+   oxSerializationTypes: oxTSerializationTypes;
+
+IMPLEMENTATION
+
+procedure init();
+begin
+   oxSerializationTypes.tPoint.InitRecord(SizeOf(oxTPoint));
+   oxSerializationTypes.tPoint.Name := 'oxTPoint';
+
+   oxSerializationTypes.tDimensions.InitRecord(SizeOf(oxTDimensions));
+   oxSerializationTypes.tDimensions.Name := 'oxTDimensions';
+
+   oxSerializationTypes.tPointf.InitRecord(SizeOf(oxTPointf));
+   oxSerializationTypes.tPointf.Name := 'oxTPointf';
+
+   oxSerializationTypes.tDimensionsf.InitRecord(SizeOf(oxTDimensionsf));
+   oxSerializationTypes.tDimensionsf.Name := 'oxTDimensionsf';
+end;
+
+INITIALIZATION
+   init();
+
+END.
