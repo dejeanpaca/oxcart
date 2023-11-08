b3e4db350d99f77f2e34a22e0bfb4fd4b9292e11
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:23:42 +0100

    Component for ui platform specific functionality

---
+++ oX/units/ui/uiuPlatform.pas
@@ -0,0 +1,41 @@
+{
+   uiuUtils, platform specific UI functionality
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    19.06.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT uiuPlatform;
+
+INTERFACE
+
+   USES
+      uStd;
+
+TYPE
+   { uiTPlatformComponent }
+
+   uiTPlatformComponent = class
+      {Check if the system is currently using a dark theme.
+      Returns 0 for no, 1 for yes, and -1 if unknown}
+      function IsSystemDarkTheme(): loopint; virtual;
+      {get the name of the system theme}
+      function GetSystemTheme(): string; virtual;
+   end;
+
+IMPLEMENTATION
+
+{ uiTPlatformComponent }
+
+function uiTPlatformComponent.IsSystemDarkTheme(): loopint;
+begin
+   Result := -1;
+end;
+
+function uiTPlatformComponent.GetSystemTheme(): string;
+begin
+   Result := 'unknown';
+end;
+
+END.
