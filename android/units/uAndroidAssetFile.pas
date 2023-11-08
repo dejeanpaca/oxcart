75cbb019ca62a49ec6dd1205316227d98cf678f2
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 22:00:14 +0100

    Some android asset file implementation

--- android/units/uAndroidAssetFile.pas
+++ android/units/uAndroidAssetFile.pas
@@ -9,12 +9,57 @@ UNIT uAndroidAssetFile;
 INTERFACE
 
    USES
-      uFile;
+      BaseUnix,
+      asset_manager,
+      uUnix, uFile, ufhUnix;
 
 TYPE
-   TAndroidAssetFileHandler = object(TFileHandler)
+
+   { TAndroidAssetFileHandler }
+
+   TAndroidAssetFileHandler = object(TUnixFileHandler)
+      constructor Create();
+
+      procedure Open(var f: TFile); virtual;
+      procedure Close(var f: TFile); virtual;
+      procedure OnBufferSet(var f: TFile); virtual;
    end;
 
+VAR
+   androidAssetFileHandler: TAndroidAssetFileHandler;
+
 IMPLEMENTATION
+
+{ TAndroidAssetFileHandler }
+
+constructor TAndroidAssetFileHandler.Create();
+begin
+   Name := 'android_asset';
+   UseBuffering := false;
+end;
+
+procedure TAndroidAssetFileHandler.Open(var f: TFile);
+begin
+   f.AutoSetBuffer();
+end;
+
+procedure TAndroidAssetFileHandler.Close(var f: TFile);
+begin
+   if(f.Handle > 0) then begin
+      FpClose(f.Handle);
+      unxfIoErr(f);
+   end;
+
+   if(f.ExtData <> nil) then
+      AAsset_close(PAAsset(f.ExtData));
+end;
+
+procedure TAndroidAssetFileHandler.OnBufferSet(var f: TFile);
+begin
+   {NOTE: We don't support buffered handler swapping here}
+end;
+
+INITIALIZATION
+   androidAssetFileHandler.Create();
 
 END.
