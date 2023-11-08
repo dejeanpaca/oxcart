2da85247f1afd91e7bdc6d2e8061a402619f19a5
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:56:02 +0100

    Start integrating YPK with OXED

--- oX/oxed/units/build/oxeduYPK.pas
+++ oX/oxed/units/build/oxeduYPK.pas
@@ -9,8 +9,38 @@ UNIT oxeduYPK;
 INTERFACE
 
    USES
-      ypkuFS, ypkuBuilder;
+      {ypk}
+      ypkuFS, ypkuBuilder,
+      {oxed}
+      uOXED, oxeduBuild;
+
+TYPE
+   oxedTYPK = record
+      Builder: ypkTBuilder;
+   end;
+
+VAR
+   oxedYPK: oxedTYPK;
 
 IMPLEMENTATION
+
+procedure onAssets();
+begin
+   oxedYPK.Builder.Reset();
+end;
+
+procedure initialize();
+begin
+   ypkTBuilder.Initialize(oxedYPK.Builder);
+end;
+
+procedure deinitialize();
+begin
+   oxedYPK.Builder.Dispose();
+end;
+
+INITIALIZATION
+   oxed.Init.Add('ypk', @initialize, @deinitialize);
+   oxedBuild.OnAssets.Add(@onAssets);
 
 END.
