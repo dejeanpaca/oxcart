ea108917dce8bdafd7ec91a91de03074b947a327
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:52:19 +0100

    Expand android main to quit on back, or when app is not active

--- oX/units/platforms/android/oxuAndroidMain.pas
+++ oX/units/platforms/android/oxuAndroidMain.pas
@@ -9,24 +9,51 @@ UNIT oxuAndroidMain;
 INTERFACE
 
 USES
-   uLog,
-   jni, looper, android_native_app_glue;
+   ctypes, looper, input, android_native_app_glue, android_keycodes,
+   uLog, ulogAndroid, StringUtils,
+   uApp, appuActionEvents;
 
 procedure android_main(app: Pandroid_app); cdecl;
 
 IMPLEMENTATION
 
-procedure handle_cmd(app: Pandroid_app; cmd: int32); cdecl;
+procedure handle_cmd(app: Pandroid_app; cmd: cint32); cdecl;
 begin
+   logi('command: ' + sf(command));
+end;
+
+function handle_input(app: Pandroid_app; event: PAInputEvent): cint32; cdecl;
+var
+   kc,
+   action: cint32;
+
+begin
+   kc := AKeyEvent_getKeyCode(event);
+   action := AKeyEvent_getAction(event);
+
+   if(kc = AKEYCODE_BACK) then begin
+      if(action = AKEY_STATE_UP) then begin
+         uApp.app.Active := false;
+         appActionEvents.QueueQuitEvent();
+
+         exit(1);
+      end;
+   end;
+
+   Result := 0;
 end;
 
 procedure android_main(app: Pandroid_app); cdecl;
 var
-   nEvents: int32;
+   nEvents: cint;
    pSource: Pandroid_poll_source;
 
 begin
    app^.onAppCmd := @handle_cmd;
+   app^.onInputEvent := @handle_input;
+
+   nEvents := 0;
+   pSource := nil;
 
    repeat
       if(ALooper_pollAll(0, nil, @nEvents, @pSource) >= 0) then begin
@@ -34,7 +61,7 @@ begin
             pSource^.process(app, pSource);
          end;
       end;
-   until (app^.destroyRequested > 0);
+   until (app^.destroyRequested > 0) or (not uApp.app.Active);
 end;
 
 END.
