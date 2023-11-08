0dc54301aa1b70a17532be8538335a3da41276ed
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 22:19:05 +0100

    Add a unxFpOpen() method

--- units/linux/uUnix.pas
+++ units/linux/uUnix.pas
@@ -11,17 +11,33 @@ INTERFACE
    USES
       unix, baseunix;
 
+CONST
+   { read/write permission for everyone }
+   MODE_FPOPEN = S_IWUSR OR S_IRUSR OR
+                S_IWGRP OR S_IRGRP OR
+                S_IWOTH OR S_IROTH;
+
 TYPE
    {a read/write fd pipe descriptor}
    unxTPipe = array[0..1] of cint;
 
+function unxFpOpen(path: RawByteString; flags: cInt; Mode: TMode): cInt;
 function unxFpread(fd: cint; buf: pchar; nbytes: TSize): TSsize; external name 'FPC_SYSC_READ';
-function unxFpWrite(fd : cInt; buf: pChar; nbytes: TSize): TSsize;  external name 'FPC_SYSC_WRITE';
+function unxFpWrite(fd: cInt; buf: pChar; nbytes: TSize): TSsize;  external name 'FPC_SYSC_WRITE';
 
 {helper method to more easily initialize a pipe (via FpPipe)}
 function unxFpPipe(out pipe: unxTPipe): cint;
 
 IMPLEMENTATION
+
+function unxFpOpen(path: RawByteString; flags: cInt; Mode: TMode): cInt;
+var
+   SystemPath: RawByteString;
+
+Begin
+   SystemPath:=ToSingleByteFileSystemEncodedFileName(path);
+   Result := FpOpen(pchar(SystemPath), flags, Mode);
+end;
 
 function unxFpPipe(out pipe: unxTPipe): cint;
 begin
