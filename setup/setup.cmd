7beace22eb61967190719fc3be2e98abc6f6148a
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:32:16 +0100

    Check if lazbuild is present in path before executing setup

--- setup/setup.cmd
+++ setup/setup.cmd
@@ -1,2 +1,16 @@
+@echo off
+
+REM Check if we have lazbuild
+where /q lazbuild
+
+if ERRORLEVEL 0 (
+    echo "Found lazbuild"
+) else (
+    echo "Could not find lazbuild, check if it is set in your PATH."
+    EXIT /B
+)
+
+EXIT /B
+
 @lazbuild setup.lpi
 @if %ERRORLEVEL% EQU 0 setup.exe %*
