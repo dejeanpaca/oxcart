8dfa65aef366a4def6c45e6274bb6820aa9fd955
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:37:00 +0100

    UI Files settings now have a global directory color

--- oX/units/ui/uiuFiles.pas
+++ oX/units/ui/uiuFiles.pas
@@ -9,7 +9,7 @@ UNIT uiuFiles;
 INTERFACE
 
    USES
-      udvars, uStd, uFileUtils,
+      udvars, uStd, uColors, uFileUtils,
       {ox}
       uiuUI;
 
@@ -22,6 +22,8 @@ TYPE
       ShowHiddenFiles,
       {should we send files to the trash/recycle location}
       UseTrash: boolean;
+
+      DirectoryColor: TColor4ub;
 
       {sort files}
       procedure SortFiles(var files: TFileDescriptorList);
@@ -52,6 +54,7 @@ INITIALIZATION
    uiFiles.SortFoldersFirst := true;
    uiFiles.Sort := true;
    uiFiles.UseTrash := true;
+   uiFiles.DirectoryColor.Assign(255, 206, 0, 255);
 
    uiTUI.dvg.Add(dvSortFiles, 'sort_files', dtcBOOL, @uiFiles.Sort);
    uiTUI.dvg.Add(dvSortFoldersFirst, 'sort_folders_first', dtcBOOL, @uiFiles.SortFoldersFirst);
