6151443d2c2021bff0457ba126672ec758e994bb
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:35:20 +0100

    Dialog to find android sdk for the android settings tab

--- oX/oxed/units/android/oxeduwndAndroidGeneralSettings.pas
+++ oX/oxed/units/android/oxeduwndAndroidGeneralSettings.pas
@@ -11,13 +11,15 @@ UNIT oxeduwndAndroidGeneralSettings;
 INTERFACE
 
    USES
+      uStd, StringUtils,
       {ox}
-      oxuRunRoutines,
+      oxuRunRoutines, oxuTypes,
       {widgets}
-      uiWidgets, wdguCheckbox, wdguDivisor, wdguInputBox, wdguLabel,
+      oxuwndSettings, oxuwndFileDialog,
+      uiWidgets, wdguCheckbox, wdguDivisor, wdguInputBox, wdguLabel, wdguButton,
       {oxed}
       uOXED,
-      oxeduAndroidSettings, oxuwndSettings;
+      oxeduAndroidSettings;
 
 IMPLEMENTATION
 
@@ -26,6 +28,30 @@ VAR
       SDKPath: wdgTInputBox;
    end;
 
+   dlgSDKPath: oxTFileDialog;
+
+procedure openSDKPathCallback(dialog: oxTFileDialog);
+var
+   path: StdString;
+
+begin
+   if(not dialog.Canceled) then begin
+      path := IncludeTrailingPathDelimiterNonEmpty(dialog.SelectedFile);
+
+      wdg.SDKPath.SetText(path);
+   end;
+end;
+
+procedure openSDKPath();
+begin
+   if(dlgSDKPath = nil) then begin
+      dlgSDKPath := oxFileDialog.OpenDirectories();
+      dlgSDKPath.SetTitle('Find SDK Path');
+      dlgSDKPath.Callback := @openSDKPathCallback;
+   end;
+
+   dlgSDKPath.Open();
+end;
 
 procedure saveCallback();
 begin
@@ -52,6 +78,8 @@ begin
    wdgLabel.Add('SDK Path');
    wdg.SDKPath := wdgInputBox.Add('');
    wdg.SDKPath.SetPlaceholder('SDK Path');
+
+   wdgButton.Add('Find SDK', uiWidget.LastRect.RightOf(), oxNullDimensions, 0).Callback.Use(@openSDKPath);
 end;
 
 procedure init();
