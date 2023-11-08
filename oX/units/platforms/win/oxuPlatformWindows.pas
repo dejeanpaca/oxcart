3f7fdc0579e7044c2b9ad6a5850bd9d6513bb231
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:23:42 +0100

    Expand windows platform with unit that includes components

---
+++ oX/units/platforms/win/oxuPlatformWindows.pas
@@ -0,0 +1,18 @@
+{
+   Windows OS platform includes
+   Copyright (c) 2019. Dejan Boras
+
+   Started On:    19.06.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuPlatformWindows;
+
+INTERFACE
+
+   USES
+      oxuWindowsPlatform, uiuwinPlatform;
+
+IMPLEMENTATION
+
+END.
---
+++ oX/units/platforms/win/uiuwinPlatform.pas
@@ -0,0 +1,73 @@
+{
+   ui windows platform functionality
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    19.06.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT uiuwinPlatform;
+
+INTERFACE
+
+   USES
+      uStd,
+      uOX, oxuPlatform, oxuPlatforms, oxuRunRoutines,
+      oxuWindowsPlatform, uiuPlatform;
+
+TYPE
+   { uiTWindowsPlatformComponent }
+
+   uiTWindowsPlatformComponent = class(uiTPlatformComponent)
+      {Check if the system is currently using a dark theme.
+      Returns 0 for no, 1 for yes, and -1 if unknown}
+      function IsSystemDarkTheme(): loopint; override;
+      {get the name of the system theme}
+      function GetSystemTheme(): string; override;
+   end;
+
+VAR
+  uiWindowsPlatform: uiTWindowsPlatformComponent;
+
+IMPLEMENTATION
+
+{ uiTWindowsPlatformComponent }
+
+function uiTWindowsPlatformComponent.IsSystemDarkTheme(): loopint;
+begin
+   Result := -1;
+end;
+
+function uiTWindowsPlatformComponent.GetSystemTheme(): string;
+begin
+   Result := 'unknown';
+end;
+
+function componentReturn(): TObject;
+begin
+   Result := uiWindowsPlatform;
+end;
+
+VAR
+   initRoutines: oxTRunRoutine;
+
+procedure init();
+var
+    p: oxTWindowsPlatform;
+
+begin
+   uiWindowsPlatform := uiTWindowsPlatformComponent.Create();
+
+   p := oxTWindowsPlatform(oxPlatforms.Find(oxTWindowsPlatform));
+   p.Components.RegisterComponent('ui.platform', @componentReturn);
+end;
+
+procedure deinit();
+begin
+   FreeObject(uiWindowsPlatform);
+end;
+
+INITIALIZATION
+   oxPlatforms.OnComponent.Add(initRoutines, 'ui.win.platform', @init, @deinit);
+
+END.
