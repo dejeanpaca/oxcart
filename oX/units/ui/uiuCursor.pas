fff04c85be685ee9404cd246a2233db97ffcff55
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:48:49 +0100

    Separate cursor renderer into its own unit

--- oX/units/ui/uiuCursor.pas
+++ oX/units/ui/uiuCursor.pas
@@ -16,7 +16,7 @@ INTERFACE
       uOX, oxuRunRoutines,
       oxuPlatform, oxuTexture, oxuTextureGenerate, oxuTypes, oxuTransform, oxuRender,
       {ui}
-      oxuUI, uiuTypes, uiuControl, uiuWindow, uiuWindowTypes, uiuDraw;
+      oxuUI, uiuTypes, uiuControl, uiuWindowTypes, uiuDraw;
 
 TYPE
    uiTLoadedCursors = specialize TSimpleList<uiTCursorType>;
@@ -113,49 +113,9 @@ begin
    SetCursorTypeForced(cursorType);
 end;
 
-procedure renderCursor({%H-}wnd: uiTWindow);
-var
-   size: oxTDimensions;
-   tex: oxTTexture;
-
-begin
-   if(uiCursor.CustomCursor > 0) and (uiCursor.CustomCursor <= uiCursor.CustomCursors.n) then begin
-      tex := uiCursor.CustomCursors.List[uiCursor.CustomCursor - 1];
-      if(tex.rId = 0) then
-         exit;
-
-      {get size from texture or from overriden size}
-      if(uiCursor.CursorSize.w > 0) then
-         size.w := uiCursor.CursorSize.w
-      else
-         size.w := tex.Width;
-
-      if(uiCursor.CursorSize.h > 0) then
-         size.h := uiCursor.CursorSize.h
-      else
-         size.h := tex.Height;
-
-      {move into position}
-      oxTransform.Identity();
-      oxTransform.Translate(oxui.mSelect.startPoint.x + (size.w / 2), oxui.mSelect.startPoint.y - (size.h / 2), 0);
-      oxTransform.Scale(size.w / 2, size.h / 2, 0);
-      oxTransform.Apply();
-
-      {bind texture}
-      oxRender.EnableBlend();
-      uiDraw.Color(1.0, 1.0, 1.0, 1.0);
-      uiDraw.Quad(tex);
-
-      oxTransform.Identity();
-      oxTransform.Apply();
-   end;
-end;
-
 procedure init();
 begin
    uiCursor.SetCursorTypeForced(uiCURSOR_TYPE_DEFAULT);
-
-   uiWindow.OxwPostRender.Add(@renderCursor);
 end;
 
 procedure deinit();
---
+++ oX/units/ui/uiuCursorRenderer.pas
@@ -0,0 +1,66 @@
+{
+   uiuCursorRenderer, custom cursor renderer
+   Copyright (C) 2020. Dejan Boras
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT uiuCursorRenderer;
+
+INTERFACE
+
+   USES
+      uStd,
+      {ox}
+      uOX, oxuTypes, oxuTexture, oxuTransform, oxuRender,
+      {ui}
+      oxuUI, uiuWindowTypes, uiuWindow, uiuDraw, uiuCursor;
+
+IMPLEMENTATION
+
+procedure renderCursor({%H-}wnd: uiTWindow);
+var
+   size: oxTDimensions;
+   tex: oxTTexture;
+
+begin
+   if(uiCursor.CustomCursor > 0) and (uiCursor.CustomCursor <= uiCursor.CustomCursors.n) then begin
+      tex := uiCursor.CustomCursors.List[uiCursor.CustomCursor - 1];
+      if(tex.rId = 0) then
+         exit;
+
+      {get size from texture or from overriden size}
+      if(uiCursor.CursorSize.w > 0) then
+         size.w := uiCursor.CursorSize.w
+      else
+         size.w := tex.Width;
+
+      if(uiCursor.CursorSize.h > 0) then
+         size.h := uiCursor.CursorSize.h
+      else
+         size.h := tex.Height;
+
+      {move into position}
+      oxTransform.Identity();
+      oxTransform.Translate(oxui.mSelect.startPoint.x + (size.w / 2), oxui.mSelect.startPoint.y - (size.h / 2), 0);
+      oxTransform.Scale(size.w / 2, size.h / 2, 0);
+      oxTransform.Apply();
+
+      {bind texture}
+      oxRender.EnableBlend();
+      uiDraw.Color(1.0, 1.0, 1.0, 1.0);
+      uiDraw.Quad(tex);
+
+      oxTransform.Identity();
+      oxTransform.Apply();
+   end;
+end;
+
+procedure init();
+begin
+   uiWindow.OxwPostRender.Add(@renderCursor);
+end;
+
+INITIALIZATION
+   ox.Init.Add('ui.cursor.render', @init);
+
+END.
