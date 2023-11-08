ab1062ffd0f8d22ad3ad18cd0519e207e74b062a
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:44:55 +0100

    Have a separate unit for a skybox

---
+++ oX/units/render/oxuSkybox.pas
@@ -0,0 +1,21 @@
+{
+   oxuSkybox, sky box
+   Copyright (c) 2020. Dejan Boras
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuSkybox;
+
+INTERFACE
+
+   USES
+      uStd;
+
+TYPE
+   oxTSkybox = record
+      Name: StdString;
+   end;
+
+IMPLEMENTATION
+
+END.
