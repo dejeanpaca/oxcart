4c94da670218b5cb9d42458711cdb81ae804361d
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:59:39 +0100

    Improve android support for modifying layout parameters

--- android/units/android_window.pas
+++ android/units/android_window.pas
@@ -10,7 +10,7 @@ INTERFACE
 
    USES
       ctypes, jni, native_activity,
-      native_window, uAndroid;
+      native_window, uAndroid, android_layout_params;
 
 TYPE
 
@@ -24,12 +24,16 @@ TYPE
 
       getWindow,
       getDecorView,
-      setSystemUiVisibilityID: jmethodID;
+      setSystemUiVisibilityID,
+      getAttributesID,
+      setAttributesID: jmethodID;
 
       {get window from given activity}
       function Get(from: PANativeActivity): jclass;
       {set UI visibility flags}
-      procedure SetSystemUiVisibility(on: PANativeActivity; flags: jint);
+      procedure SetSystemUiVisibility(activity: PANativeActivity; flags: jint);
+      {set layout in display cutout mode}
+      procedure SetLayoutInDisplayCutoutMode(activity: PANativeActivity; layout: jint);
 
       {initialize jni stuff we need}
       procedure Initialize();
@@ -47,19 +51,35 @@ begin
    Result := mainThreadEnv^^.CallObjectMethod(mainThreadEnv, from^.clazz, getWindow);
 end;
 
-procedure androidTWindow.SetSystemUiVisibility(on: PANativeActivity; flags: jint);
+procedure androidTWindow.SetSystemUiVisibility(activity: PANativeActivity; flags: jint);
 var
    env: PJNIEnv;
    wnd: jclass;
-   decorView: jmethodID;
+   decorView: jobject;
 
 begin
    env := mainThreadEnv;
 
-   wnd := Get(on);
+   wnd := Get(activity);
    decorView := env^^.CallObjectMethod(env, wnd, getDecorView);
 
    env^^.CallVoidMethodA(env, decorView, setSystemUiVisibilityID, @flags);
+end;
+
+procedure androidTWindow.SetLayoutInDisplayCutoutMode(activity: PANativeActivity; layout: jint);
+var
+   wnd: jclass;
+   attributes: jobject;
+   args: array[0..0] of jvalue;
+
+begin
+   wnd := Get(activity);
+   attributes := mainThreadEnv^^.CallObjectMethod(mainThreadEnv, wnd, getAttributesID);
+
+   androidLayoutParams.SetLayoutInDisplayCutoutMode(attributes, LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES);
+
+   args[0].l := attributes;
+   mainThreadEnv^^.CallVoidMethodA(mainThreadEnv, wnd, setAttributesID, args);
 end;
 
 procedure androidTWindow.Initialize();
@@ -75,8 +95,13 @@ begin
    windowClass := env^^.FindClass(env, 'android/view/Window');
 
    getWindow := mainThreadEnv^^.GetMethodID(mainThreadEnv, activityClass, 'getWindow', '()Landroid/view/Window;');
-   getDecorView := env^^.GetMethodID(env, windowClass, 'getDecorView', '()Landroid/view/View;');
    setSystemUiVisibilityID := env^^.GetMethodID(env, viewClass, 'setSystemUiVisibility', '(I)V');
+
+   getDecorView := env^^.GetMethodID(env, windowClass, 'getDecorView', '()Landroid/view/View;');
+   getAttributesID := env^^.GetMethodID(env, windowClass, 'getAttributes', '()Landroid/view/WindowManager$LayoutParams;');
+   setAttributesID := env^^.GetMethodID(env, windowClass, 'setAttributes', '(Landroid/view/WindowManager$LayoutParams;)V');
+
+   androidLayoutParams.Initialize();
 end;
 
 END.
