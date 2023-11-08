c3c15fd99d05ce385f3cef6e4d1172f0d1eacd05
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:59:36 +0100

    Clean up native activity helper

--- android/units/android_native_activity_helper.pas
+++ android/units/android_native_activity_helper.pas
@@ -9,10 +9,11 @@ UNIT android_native_activity_helper;
 INTERFACE
 
    USES
-      ctypes, jni, android_native_app_glue, android_env,
-      uAndroid;
+      ctypes, jni, android_native_app_glue,
+      uAndroid, android_view;
 
 procedure androidAutoHideNavBar(var app: android_app);
+function androidGetWindow(var app: android_app): jclass;
 
 IMPLEMENTATION
 
@@ -20,29 +21,21 @@ VAR
    activityClass,
    windowClass,
    viewClass,
-   layoutParamsClass,
    windowManagerClass,
-   window,
-   decorView: jclass;
+   decorView,
+   window: jclass;
 
    getWindow,
    getDecorView,
    setSystemUiVisibility: jmethodID;
 
-   flagFullscreenID,
-   flagHideNavigationID,
-   flagImmersiveStickyID,
-   flagLayoutStableID,
-   flagLayoutHideNavigationID,
-   flagLayoutFullscreenID,
-   layoutInDisplayCutoutMode: jfieldID;
+function androidGetWindow(var app: android_app): jclass;
+begin
+   if(getWindow = nil) then
+      getWindow := mainThreadEnv^^.GetMethodID(mainThreadEnv, activityClass, 'getWindow', '()Landroid/view/Window;');
 
-   flagFullscreen,
-   flagHideNavigation,
-   flagImmersiveSticky,
-   flagLayoutStable,
-   flagLayoutHideNavigation,
-   flagLayoutFullscreen: cint;
+   Result := mainThreadEnv^^.CallObjectMethod(mainThreadEnv, app.activity^.clazz, getWindow);
+end;
 
 procedure androidAutoHideNavBar(var app: android_app);
 var
@@ -54,7 +47,6 @@ begin
 
    if(activityClass = nil) then begin
       activityClass := env^^.FindClass(env, 'android/app/NativeActivity');
-      getWindow := env^^.GetMethodID(env, activityClass, 'getWindow', '()Landroid/view/Window;');
 
       windowClass := env^^.FindClass(env, 'android/view/Window');
       getDecorView := env^^.GetMethodID(env, windowClass, 'getDecorView', '()Landroid/view/View;');
@@ -62,33 +54,13 @@ begin
       viewClass := env^^.FindClass(env, 'android/view/View');
       windowManagerClass := env^^.FindClass(env, 'android/view/WindowManager');
       setSystemUiVisibility := env^^.GetMethodID(env, viewClass, 'setSystemUiVisibility', '(I)V');
-
-      layoutParamsClass := env^^.FindClass(env, 'android/view/WindowManager$LayoutParams');
-
-      if(android_api_level >= 28) then begin
-         layoutInDisplayCutoutMode := env^^.GetFieldID(env, layoutParamsClass, 'layoutInDisplayCutoutMode', 'I');
-      end;
-
-      flagFullscreenID := env^^.GetStaticFieldID(env, viewClass, 'SYSTEM_UI_FLAG_FULLSCREEN', 'I');
-      flagHideNavigationID := env^^.GetStaticFieldID(env, viewClass, 'SYSTEM_UI_FLAG_HIDE_NAVIGATION', 'I');
-      flagImmersiveStickyID := env^^.GetStaticFieldID(env, viewClass, 'SYSTEM_UI_FLAG_IMMERSIVE_STICKY', 'I');
-      flagLayoutStableID := env^^.GetStaticFieldID(env, viewClass, 'SYSTEM_UI_FLAG_LAYOUT_STABLE', 'I');
-      flagLayoutHideNavigationID := env^^.GetStaticFieldID(env, viewClass, 'SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION', 'I');
-      flagLayoutFullscreenID := env^^.GetStaticFieldID(env, viewClass, 'SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN', 'I');
-
-      flagFullscreen := env^^.GetStaticIntField(env, viewClass, flagFullscreenID);
-      flagHideNavigation := env^^.GetStaticIntField(env, viewClass, flagHideNavigationID);
-      flagImmersiveSticky := env^^.GetStaticIntField(env, viewClass, flagImmersiveStickyID);
-      flagLayoutStable := env^^.GetStaticIntField(env, viewClass, flagLayoutStableID);
-      flagLayoutHideNavigation := env^^.GetStaticIntField(env, viewClass, flagLayoutHideNavigationID);
-      flagLayoutFullscreen := env^^.GetStaticIntField(env, viewClass, flagLayoutFullscreenID);
    end;
 
-   flag := flagImmersiveSticky or
-      flagLayoutStable or flagLayoutHideNavigation or flagLayoutFullscreen or
-      flagHideNavigation or flagFullscreen;
+   flag := SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+      SYSTEM_UI_FLAG_LAYOUT_STABLE or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+      SYSTEM_UI_FLAG_HIDE_NAVIGATION or SYSTEM_UI_FLAG_FULLSCREEN;
 
-   window := env^^.CallObjectMethod(env, app.activity^.clazz, getWindow);
+   window := androidGetWindow(app);
    decorView := env^^.CallObjectMethod(env, window, getDecorView);
 
    env^^.CallVoidMethodA(env, decorView, setSystemUiVisibility, @flag);
