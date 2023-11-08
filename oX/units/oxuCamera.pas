fb75f8b1820f8abcaab60dd7b36452870231c1e3
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:24:51 +0100

    Separate camera input code into its own unit

--- oX/units/oxuCamera.pas
+++ oX/units/oxuCamera.pas
@@ -12,12 +12,8 @@ INTERFACE
 
    USES
       Math, uStd, vmVector, vmMath,
-      {app}
-      appuMouse,
       {ox}
-      oxuTransform, oxuSerialization, oxuProjectionType, oxuProjection,
-      {ui}
-      uiuWindowTypes, uiuWindow;
+      oxuTransform, oxuSerialization, oxuProjectionType, oxuProjection;
 
 CONST
    oxvCameraPosition: TVector3  = (0.0, 0.0, 0.0);
@@ -86,24 +82,6 @@ TYPE
       procedure GetPointerOrigin(x, y: single; out origin: TVector3f; const projection: oxTProjection);
       {get ray from pointer position}
       procedure GetPointerRay(x, y: single; out origin, endPosition: TVector3f; const projection: oxTProjection);
-   end;
-
-   { oxTCameraCursorControl }
-
-   oxTCameraCursorControl = record
-      {last pointer position}
-      LastPointerPosition: TVector2f;
-      {cursor angle spedd}
-      CursorAngleSpeed: Single;
-
-      {start cursor control}
-      procedure Start();
-      {cursor control}
-      procedure GetPointerMovement(wnd: uiTWindow; var camera: oxTCamera; out mx, my: single; center: boolean = true);
-      {cursor control}
-      procedure Control(wnd: uiTWindow; var camera: oxTCamera; center: boolean = true);
-      {cursor control}
-      procedure OrbitControl(wnd: uiTWindow; var camera: oxTCamera; center: boolean = true);
    end;
 
 VAR
@@ -308,86 +286,6 @@ begin
    vRight := oxvCameraRight;
 
    Transform := oxTTransform.Instance();
-end;
-
-procedure oxTCameraCursorControl.Start();
-begin
-   appm.GetPosition(nil, LastPointerPosition[0], LastPointerPosition[1]);
-end;
-
-procedure oxTCameraCursorControl.GetPointerMovement(wnd: uiTWindow; var camera: oxTCamera; out mx, my: single; center: boolean);
-var
-   ox,
-   oy,
-   nx,
-   ny: single;
-
-begin
-   mx := 0;
-   my := 0;
-
-   ox := 0;
-   oy := 0;
-
-   if(center) then
-      appm.GetPosition(nil, ox, oy);
-
-   if(ox <> LastPointerPosition[0]) or (oy <> LastPointerPosition[1]) then begin
-      if(center) then
-         wnd.SetPointerCentered()
-      else begin
-         ox := LastPointerPosition[0];
-         oy := LastPointerPosition[1];
-      end;
-
-      appm.GetPosition(nil, nx, ny);
-      if(center) then begin
-         mx := nx - ox;
-         my := ny - oy;
-      end else begin
-         mx := ox - nx;
-         my := oy - ny;
-      end;
-
-      LastPointerPosition[0] := nx;
-      LastPointerPosition[1] := ny;
-   end;
-end;
-
-procedure oxTCameraCursorControl.Control(wnd: uiTWindow; var camera: oxTCamera; center: boolean);
-var
-   mx,
-   my: single;
-
-begin
-   GetPointerMovement(wnd, camera, mx, my, center);
-
-   if(mx <> 0) or (my <> 0) then begin
-      Camera.IncPitchYaw(my / CursorAngleSpeed, mx / CursorAngleSpeed);
-   end;
-end;
-
-procedure oxTCameraCursorControl.OrbitControl(wnd: uiTWindow; var camera: oxTCamera; center: boolean);
-var
-   mx,
-   my: single;
-
-begin
-   GetPointerMovement(wnd, camera, mx, my, center);
-
-   if(mx <> 0) or (my <> 0) then begin
-      vmRotateAroundPoint(mx / CursorAngleSpeed / 3.14, 0, 1, 0, vmvZero3f, Camera.vPos);
-      vmRotateAroundPoint(my / CursorAngleSpeed / 3.14, 1, 0, 0, vmvZero3f, Camera.vPos);
-
-      vmRotateAroundPoint(mx / CursorAngleSpeed / 3.14, 0, 1, 0, vmvZero3f, Camera.vUp);
-      vmRotateAroundPoint(my / CursorAngleSpeed / 3.14, 1, 0, 0, vmvZero3f, Camera.vUp);
-      Camera.vUp.Normalize();
-
-      Camera.vView := (vmvZero3f - Camera.vPos);
-      Camera.vView.Normalize();
-
-      Camera.vRight := Camera.vView.Cross(Camera.vUp);
-   end;
 end;
 
 INITIALIZATION
---
+++ oX/units/oxuCameraInput.pas
@@ -0,0 +1,124 @@
+{
+   oxuCameraInput, camera input control
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    11.07.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxuCameraInput;
+
+INTERFACE
+
+   USES
+      uStd, vmVector,
+      {app}
+      appuMouse,
+      {ox}
+      oxuCamera,
+      {ui}
+      uiuWindowTypes, uiuWindow;
+
+TYPE
+   { oxTCameraCursorControl }
+
+   oxTCameraCursorControl = record
+      {last pointer position}
+      LastPointerPosition: TVector2f;
+      {cursor angle spedd}
+      CursorAngleSpeed: Single;
+
+      {start cursor control}
+      procedure Start();
+      {cursor control}
+      procedure GetPointerMovement(wnd: uiTWindow; out mx, my: single; center: boolean = true);
+      {cursor control}
+      procedure Control(wnd: uiTWindow; var camera: oxTCamera; center: boolean = true);
+      {cursor control}
+      procedure OrbitControl(wnd: uiTWindow; var camera: oxTCamera; center: boolean = true);
+   end;
+
+
+IMPLEMENTATION
+
+procedure oxTCameraCursorControl.Start();
+begin
+   appm.GetPosition(nil, LastPointerPosition[0], LastPointerPosition[1]);
+end;
+
+procedure oxTCameraCursorControl.GetPointerMovement(wnd: uiTWindow; out mx, my: single; center: boolean);
+var
+   ox,
+   oy,
+   nx,
+   ny: single;
+
+begin
+   mx := 0;
+   my := 0;
+
+   ox := 0;
+   oy := 0;
+
+   if(center) then
+      appm.GetPosition(nil, ox, oy);
+
+   if(ox <> LastPointerPosition[0]) or (oy <> LastPointerPosition[1]) then begin
+      if(center) then
+         wnd.SetPointerCentered()
+      else begin
+         ox := LastPointerPosition[0];
+         oy := LastPointerPosition[1];
+      end;
+
+      appm.GetPosition(nil, nx, ny);
+      if(center) then begin
+         mx := nx - ox;
+         my := ny - oy;
+      end else begin
+         mx := ox - nx;
+         my := oy - ny;
+      end;
+
+      LastPointerPosition[0] := nx;
+      LastPointerPosition[1] := ny;
+   end;
+end;
+
+procedure oxTCameraCursorControl.Control(wnd: uiTWindow; var camera: oxTCamera; center: boolean);
+var
+   mx,
+   my: single;
+
+begin
+   GetPointerMovement(wnd, mx, my, center);
+
+   if(mx <> 0) or (my <> 0) then begin
+      Camera.IncPitchYaw(my / CursorAngleSpeed, mx / CursorAngleSpeed);
+   end;
+end;
+
+procedure oxTCameraCursorControl.OrbitControl(wnd: uiTWindow; var camera: oxTCamera; center: boolean);
+var
+   mx,
+   my: single;
+
+begin
+   GetPointerMovement(wnd, mx, my, center);
+
+   if(mx <> 0) or (my <> 0) then begin
+      vmRotateAroundPoint(mx / CursorAngleSpeed / 3.14, 0, 1, 0, vmvZero3f, Camera.vPos);
+      vmRotateAroundPoint(my / CursorAngleSpeed / 3.14, 1, 0, 0, vmvZero3f, Camera.vPos);
+
+      vmRotateAroundPoint(mx / CursorAngleSpeed / 3.14, 0, 1, 0, vmvZero3f, Camera.vUp);
+      vmRotateAroundPoint(my / CursorAngleSpeed / 3.14, 1, 0, 0, vmvZero3f, Camera.vUp);
+      Camera.vUp.Normalize();
+
+      Camera.vView := (vmvZero3f - Camera.vPos);
+      Camera.vView.Normalize();
+
+      Camera.vRight := Camera.vView.Cross(Camera.vUp);
+   end;
+end;
+
+END.
