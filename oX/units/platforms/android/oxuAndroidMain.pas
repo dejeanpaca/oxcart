2f640a5b60a3114fd167e08ffae8a37bd5aa4373
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:58:58 +0100

    Rework how android platform handles main thread
    - Initialize env and looper via onAndroidAppCreate
    - Get a main thread looper and handle signals via the looper
    - Use fSignalMainThread flag to signal operations which need to be done on the main thread

--- oX/units/platforms/android/oxuAndroidMain.pas
+++ oX/units/platforms/android/oxuAndroidMain.pas
@@ -9,8 +9,9 @@ UNIT oxuAndroidMain;
 INTERFACE
 
 USES
-   android_native_app_glue, android_log_helper, native_activity, android_native_activity_helper,
-   StringUtils,
+   android_native_app_glue, android_log_helper, native_activity, android_native_activity_helper, android_env,
+   ctypes, looper, jni,
+   uLog, uUnix, StringUtils,
    {app}
    uApp,
    {ox}
@@ -19,6 +20,52 @@ USES
 procedure android_main(app: Pandroid_app); cdecl;
 
 IMPLEMENTATION
+
+VAR
+   mainThreadLooper: PALooper;
+   mainThreadMessagePipe: unxTPipe;
+
+function mainThreadLooperCallback(fd: cint; events: cint; data: pointer): cint; cdecl;
+var
+   msg: char;
+
+begin
+   unxFpread(fd, @msg, 1);
+   Result := 1;
+
+   if(AndroidApp = nil) or (msg <> '@') then
+      exit;
+
+   if(oxAndroidPlatform.fHideNavbar) then begin
+      androidAutoHideNavBar(AndroidApp^);
+      oxAndroidPlatform.fHideNavbar := false;
+   end;
+end;
+
+procedure signalMainThread();
+var
+   msg: char;
+
+begin
+   msg := '@';
+
+   unxFpWrite(mainThreadMessagePipe[1], @msg, 1);
+end;
+
+procedure getMainThreadLooper();
+begin
+   {setup a message pipe for the UI thread}
+   mainThreadLooper := ALooper_forThread();
+
+   if(mainThreadLooper <> nil) then begin
+      ALooper_acquire(mainThreadLooper);
+      unxFpPipe(mainThreadMessagePipe);
+
+      ALooper_addFd(mainThreadLooper, mainThreadMessagePipe[0], 0, ALOOPER_EVENT_INPUT, @mainThreadLooperCallback, nil);
+      logv('Got main thread looper');
+   end else
+      loge('Failed to get main thread looper');
+end;
 
 procedure android_main(app: Pandroid_app); cdecl;
 var
@@ -68,9 +115,9 @@ begin
       end;
 
       if(ox.Started) then begin
-         if(oxAndroidPlatform.HideNavbar) then begin
-            androidAutoHideNavBar(app);
-            oxAndroidPlatform.HideNavbar := false;
+         if(oxAndroidPlatform.fSignalMainThread) then begin
+            oxAndroidPlatform.fSignalMainThread := false;
+            signalMainThread();
          end;
 
          if(oxAndroidPlatform.fRegainedFocus) then begin
@@ -99,5 +146,15 @@ begin
          break;
    until false;
 end;
+
+procedure androidAppCreate(var app: android_app);
+begin
+   app.activity^.vm^^.GetEnv(app.activity^.vm, @mainThreadEnv, JNI_VERSION_1_6);
+
+   getMainThreadLooper();
+end;
+
+INITIALIZATION
+   onAndroidAppCreate := @androidAppCreate;
 
 END.
