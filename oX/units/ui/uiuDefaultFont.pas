f80a68adf47de6baa637bc895c6aedc8fd257b97
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:18:47 +0100

    Unit to load a default font for UI

---
+++ oX/units/ui/uiuDefaultFont.pas
@@ -0,0 +1,75 @@
+{
+   uiuDefaultFont, default font for UI
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    04.04.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT uiuDefaultFont;
+
+INTERFACE
+
+   USES
+      uStd, uLog,
+      {ox}
+      oxuPaths, oxuFont,
+      oxuUI, oxuFreetypeFonts;
+
+TYPE
+   { uiTDefaultFontManager }
+
+   uiTDefaultFontManager = record
+      Path: string;
+      Size: loopint;
+      Font: oxTFont;
+
+      procedure Initialize();
+      procedure Deinitialize();
+   end;
+
+VAR
+   uiDefaultFont: uiTDefaultFontManager;
+
+IMPLEMENTATION
+
+{ uiTDefaultFontManager }
+
+procedure uiTDefaultFontManager.Initialize();
+var
+   assetPath: String;
+
+begin
+   assetPath := oxAssetPaths.Find(Path);
+   Font := oxFreetypeManager.CreateFont(assetPath, 14);
+
+   if(Font <> nil) then
+      log.v('ui > Loaded default font: ' + assetPath)
+   else
+      log.w('ui > Failed to load default font: ' + assetPath);
+
+   oxui.SetDefaultFont(Font);
+end;
+
+procedure uiTDefaultFontManager.Deinitialize();
+begin
+   FreeObject(Font);
+end;
+
+procedure initialize();
+begin
+   uiDefaultFont.Initialize();
+end;
+
+procedure deinitialize();
+begin
+   uiDefaultFont.Deinitialize();
+end;
+
+INITIALIZATION
+   uiDefaultFont.Path := oxPaths.Fonts + 'Inconsolata.ttf';
+   uiDefaultFont.Size := 11;
+
+   oxui.InitializationProcs.Add('default_font', @initialize, @deinitialize);
+
+END.
