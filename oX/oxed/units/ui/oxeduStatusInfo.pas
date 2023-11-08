c6a19608df99c93b8533f5210a824f7ceb3a3e6f
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:18:34 +0100

    Unit to show OXED information on the status bar

---
+++ oX/oxed/units/ui/oxeduStatusInfo.pas
@@ -0,0 +1,70 @@
+{
+   oxeduStatusInfo, status bar information
+   Copyright (C) 2019. Dejan Boras
+
+   Started On:    04.04.2019.
+}
+
+{$INCLUDE oxdefines.inc}
+UNIT oxeduStatusInfo;
+
+INTERFACE
+
+   USES
+      cmem, uStd, StringUtils,
+      {widgets}
+      uiWidgets, wdguToolbar,
+      {oxed}
+      uOX, uOXED, oxeduIcons, oxeduStatusbar;
+
+TYPE
+   oxedTStatusInfo = record
+      Wdg: record
+         Info: wdgPToolbarItem;
+      end;
+   end;
+
+VAR
+   oxedStatusInfo: oxedTStatusInfo;
+
+IMPLEMENTATION
+
+procedure SetupInfo();
+begin
+   if(oxedStatusbar.Status = nil) then
+      exit;
+end;
+
+procedure Initialize();
+begin
+   if(oxedStatusbar.Status = nil) then
+      exit;
+
+   if(oxedStatusbar.Status.Items.n > 0) then
+      oxedStatusbar.Status.AddSeparator();
+
+   oxedStatusInfo.Wdg.Info := oxedStatusbar.Status.AddCaption('0');
+
+   SetupInfo();
+end;
+
+procedure updateInfo();
+var
+   heapStatus: THeapStatus;
+
+begin
+   if(oxedStatusInfo.Wdg.Info <> nil) then begin
+      heapStatus := GetHeapStatus;
+
+      oxedStatusInfo.Wdg.Info^.Caption := sf(heapStatus.TotalFree) + '/' + sf(heapStatus.TotalAllocated) + '/' + sf(heapStatus.Unused) + '/' + sf(heapStatus.TotalAddrSpace);
+
+      oxedStatusbar.Status.ItemChanged();
+   end;
+end;
+
+INITIALIZATION
+   oxed.PostInit.iAdd('status_info', @initialize);
+
+   ox.OnRun.Add(@updateInfo);
+
+END.
