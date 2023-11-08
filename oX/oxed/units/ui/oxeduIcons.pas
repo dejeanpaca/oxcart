cd5c272454613012da038b0f5eaf3f212c96188c
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:24:37 +0100

    Expand file icons

--- oX/oxed/units/ui/oxeduIcons.pas
+++ oX/oxed/units/ui/oxeduIcons.pas
@@ -52,7 +52,21 @@ begin
    tex := oxedIcons.Create($f1c9 {file-code-o});
    oxFileIcons.Add(tex, 'pas');
    oxFileIcons.Add(tex, 'pp');
+   oxFileIcons.Add(tex, 'inc');
    oxFileIcons.Add(tex, 'lpr');
+   oxFileIcons.Add(tex, 'c');
+   oxFileIcons.Add(tex, 'h');
+   oxFileIcons.Add(tex, 'cpp');
+   oxFileIcons.Add(tex, 'hpp');
+   oxFileIcons.Add(tex, 'cs');
+   oxFileIcons.Add(tex, 'java');
+   oxFileIcons.Add(tex, 'js');
+   oxFileIcons.Add(tex, 'json');
+   oxFileIcons.Add(tex, 'php');
+   oxFileIcons.Add(tex, 'sql');
+
+   {csv}
+   oxFileIcons.Add(oxedIcons.Create($f6dd {file-image}), 'csv');
 
    {file-tex}
    tex := oxedIcons.Create($f15c {'file-text'});
@@ -65,12 +79,15 @@ begin
    oxFileIcons.Add(tex, 'tga');
    oxFileIcons.Add(tex, 'jpeg');
    oxFileIcons.Add(tex, 'jpg');
+   oxFileIcons.Add(tex, 'tiff');
+   oxFileIcons.Add(tex, 'bmp');
 
    {file-archive-o}
    tex := oxedIcons.Create($f1c6 {file-archive-o});
    oxFileIcons.Add(tex, 'zip');
    oxFileIcons.Add(tex, '7z');
    oxFileIcons.Add(tex, 'rar');
+   oxFileIcons.Add(tex, 'apk');
 
    {file-audio-o}
    tex := oxedIcons.Create($f1c7 {file-audio-o});
@@ -84,12 +101,18 @@ begin
    oxFileIcons.Add(tex, 'mp4');
    oxFileIcons.Add(tex, 'mkv');
    oxFileIcons.Add(tex, 'h264');
-   oxFileIcons.Add(tex, 'h264');
+   oxFileIcons.Add(tex, 'h265');
    oxFileIcons.Add(tex, 'wmv');
+
+   {pdf}
+   oxFileIcons.Add(oxedIcons.Create($f1c1 {file-pdf}), 'pdf');
 
    {git}
    tex := oxedIcons.Create('brands:61906' {git});
+
    oxFileIcons.Add(tex, 'gitignore');
+   oxFileIcons.Add(tex, 'gitmodules');
+   oxFileIcons.Add(tex, 'gitattributes');
 end;
 
 { oxedTIconsGlobal }
