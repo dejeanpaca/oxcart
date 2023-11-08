8ed5f3f5e993f615cdad36f8828e668f7b691006
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:22:55 +0100

    Expand cpu information in a windows system

--- components/app/units/win/appuWinSysInfo.pas
+++ components/app/units/win/appuWinSysInfo.pas
@@ -7,7 +7,10 @@ UNIT appuWinSysInfo;
 
 INTERFACE
 
-	USES windows, windowsver, appuSysInfoBase;
+	USES
+     windows, windowsver, windowsutils,
+     uStd, StringUtils,
+     appuSysInfoBase;
 
 IMPLEMENTATION
 
@@ -16,8 +19,20 @@ CONST
 
 procedure getInformation();
 var
+   i: loopint;
    si: SYSTEM_INFO;
    mem: MEMORYSTATUS;
+   proc: appTProcessorInformation;
+
+   regKey: windows.HKEY;
+   dwMHZ: windows.DWORD;
+
+   cpuName,
+   cpuVendor,
+   cpuIdentifier,
+   previousCpuName,
+   previousCpuVendor,
+   previousCpuIdentifier: string;
 
 begin
    {system name}
@@ -26,9 +41,61 @@ begin
    {get system information from windows}
    GetSystemInfo(@si);
 
+   dwMHZ := 0;
+   cpuName := '';
+   cpuVendor := '';
+
    {get the number of processors}
    appSI.nProcessors        := si.dwNumberOfProcessors;
    appSI.HasProcessorInfo   := true;
+
+   previousCpuName := '';
+   previousCpuVendor := '';
+   previousCpuIdentifier := '';
+
+   for i := 0 to appSI.nProcessors - 1 do begin;
+      regKey := windowsutils.OpenRegistryKey(HKEY_LOCAL_MACHINE, 'HARDWARE\DESCRIPTION\System\CentralProcessor\0');
+
+      appSI.AddProcessor();
+      proc := appSI.Processors[i];
+
+      cpuName := '';
+      cpuVendor := '';
+      dwMHZ := 0;
+
+      if(regKey <> 0) then begin
+         dwMHZ := GetRegistryDWord(regKey, '~MHz');
+
+         cpuName := windowsutils.GetRegistryString(regKey, 'ProcessorNameString');
+         cpuVendor := windowsutils.GetRegistryString(regKey, 'VendorIdentifier');
+         cpuIdentifier := windowsutils.GetRegistryString(regKey, 'Identifier');
+
+         {we do this so we use the same string reference, without having unneeded copies}
+         if(cpuName = previousCpuName) then
+            cpuName := previousCpuName;
+
+         if(cpuVendor = previousCpuVendor) then
+            cpuVendor := previousCpuVendor;
+
+         if(cpuIdentifier = previousCpuIdentifier) then
+            cpuIdentifier := previousCpuIdentifier;
+
+         previousCpuName := cpuName;
+         previousCpuVendor := cpuVendor;
+         previousCpuIdentifier := cpuIdentifier;
+
+         windows.RegCloseKey(regKey);
+      end;
+
+      proc.Freq := dwMHZ;
+      proc.Name := cpuName;
+      proc.ModelName := cpuIdentifier;
+      proc.Vendor := cpuVendor;
+      proc.Model := hi(si.wProcessorRevision);
+      proc.Revision := sf(lo(si.wProcessorRevision));
+
+      appSI.Processors[i] := proc;
+   end;
 
    {get information about memory}
    GlobalMemoryStatus(@mem);
