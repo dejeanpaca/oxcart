f5b8a2034118cc01610c77fde65caa4d479abdb8
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 22:19:19 +0100

    Start unit for the GLM renderer (Modern OpenGL)

---
+++ oX/units/renderers/glm/oxuRendererGLM.pas
@@ -0,0 +1,18 @@
+{
+   oxuRendererGL, Modern OpenGL Renderer
+   Copyright (C) 2021. Dejan Boras
+}
+
+{$INCLUDE oxdefines.inc}
+
+{$IFNDEF OX_RENDERER_GLM}
+   {$FATAL Included gl renderer, with no OX_RENDERER_GLM defined}
+{$ENDIF}
+
+UNIT oxuRendererGLM;
+
+INTERFACE
+
+IMPLEMENTATION
+
+END.
