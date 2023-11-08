ad7aa81ccb51a16e68a64942b34d936bfb834b7b
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:51:37 +0100

    oX setup should no longer automatically rebuild resources
    - This is already part of repo and doesn't need to be done unless changes have been made

--- oX/setup/setup.pas
+++ oX/setup/setup.pas
@@ -8,14 +8,23 @@ USES
 {$R *.res}
 
 TYPE
-   TMode = (MODE_ALL, MODE_RESOURCES, MODE_OXED, MODE_OXED_LIBRARIES, MODE_OXED_BUILD);
+   TMode = (
+      MODE_ALL,
+      MODE_RESOURCES,
+      MODE_OXED,
+      MODE_OXED_LIBRARIES,
+      MODE_OXED_BUILD
+   );
 
 VAR
    mode: TMode = MODE_ALL;
 
-function isMode(specifiedMode: TMode): boolean;
+function isMode(specifiedMode: TMode; optional: boolean = false): boolean;
 begin
-   Result := (mode = MODE_ALL) or (mode = specifiedMode);
+   if(not optional) then
+      Result := (mode = MODE_ALL) or (mode = specifiedMode)
+   else
+      Result := mode = specifiedMode;
 end;
 
 function isOXEDMode(specifiedMode: TMode): boolean;
@@ -43,13 +52,15 @@ BEGIN
    if(parameters.FindFlag('resources')) then
       mode := MODE_RESOURCES;
 
-   if(isMode(MODE_RESOURCES)) then begin
+   {build resources (optional)}
+   if(isMode(MODE_RESOURCES, true)) then begin
       log.i('Building resources');
       ProcessHelpers.RunCommand('file2code', ['-pas', '-lf', 'data/internal/font/font.tga', 'units/resources/default_font.inc', 'default_font']);
       ProcessHelpers.RunCommand('file2code', ['-pas', '-lf', 'data/internal/default.tga', 'units/resources/default_texture.inc', 'default_texture']);
       log.i('Done building resources');
    end;
 
+   {set up libraries}
    if(isOXEDMode(MODE_OXED_LIBRARIES)) then begin
       log.i('Setting up OXED Libraries');
 
@@ -63,6 +74,7 @@ BEGIN
       log.i('Done setting up OXED libraries');
    end;
 
+   {build OXED}
    if(isOXEDMode(MODE_OXED_BUILD)) then begin
       log.i('Building OXED');
       log.i('Done building OXED');
