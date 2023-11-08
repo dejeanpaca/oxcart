a105f8a55d31c083489f1dbdb6d44bab479447df
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:51:35 +0100

    Fix: Windows cmd examples are retarded

--- setup/setup.cmd
+++ setup/setup.cmd
@@ -6,8 +6,10 @@ REM Check if we have lazbuild
 if %ERRORLEVEL% EQU 0 (
     echo "Found lazbuild"
 ) else (
+    echo "Using default path"
+
     REM Set default path for lazarus
-    SET PATH="%PATH%;C:\lazarus"
+    @SET "PATH=%PATH%;C:\lazarus"
 
     REM Check if we have lazbuild again
     @lazbuild -v >nul 2>&1
