844915387b44d0a40acbd00231d0ed458298a923
Dejan Boras <de.jean.7777@protonmail.com>
2023-11-08 21:56:16 +0100

    Additional methods and cleanups for TFileStrings

--- units/uFile/uFileStrings.pas
+++ units/uFile/uFileStrings.pas
@@ -3,18 +3,25 @@
    Copyright (C) 2018. Dejan Boras
 }
 
-{$MODE OBJFPC}{$H+}{$I-}{$MODESWITCH ADVANCEDRECORDS}
+{$INCLUDE oxheader.inc}
 UNIT uFileStrings;
 
 INTERFACE
 
-   USES sysutils, uStd, StringUtils, uFile, uFileUtils, uFiles;
+   USES
+      sysutils, uStd, StringUtils,
+      uFile, uFileUtils, uFiles;
 
 TYPE
 
    { TFileStrings }
 
    TFileStrings = record
+      {load list of short strings into a packed memory structur}
+      function LoadPacked(var f: TFile; out p: TPackedStrings): fileint;
+      {Load a list of strings from given file. Returns number of strings loaded on success or fileutil error code.}
+      function LoadList(var f: TFile; out s: TStringArray): fileint;
+
       {load list of short strings into a packed memory structur}
       function LoadPacked(const fn: string; out p: TPackedStrings): fileint;
       {Load a list of strings from given file. Returns number of strings loaded on success or fileutil error code.}
@@ -28,9 +35,8 @@ IMPLEMENTATION
 
 { TFileStrings }
 
-function TFileStrings.LoadPacked(const fn: string; out p: TPackedStrings): fileint;
+function TFileStrings.LoadPacked(var f: TFile; out p: TPackedStrings): fileint;
 var
-   f: TFile;
    current: shortstring;
    index,
    totalStrings,
@@ -44,40 +50,63 @@ begin
    totalStrings := 0;
    totalSize := 0;
 
-   fFile.Init(f);
-   f.Open(fn);
+   repeat
+      f.ReadShortString(current);
+
+      if(f.Error = 0) then begin
+         inc(totalSize, Length(current));
+         inc(totalStrings);
+      end else
+         break;
+   until f.EOF();
+
    if(f.Error = 0) then begin
+      f.SeekStart();
+      index := 0;
+
+      p.Allocate(totalStrings, totalSize);
+
       repeat
-         f.ReadShortString(current);
+         f.Readln(current);
 
          if(f.Error = 0) then begin
-            inc(totalSize, Length(current));
-            inc(totalStrings);
+            p.Store(current, index);
+            inc(index);
          end else
             break;
       until f.EOF();
 
-      if(f.Error = 0) then begin
-         f.SeekStart();
-         index := 0;
+      exit(totalStrings);
+   end;
 
-         p.Allocate(totalStrings, totalSize);
+   exit(eFILE_READ);
+end;
 
-         repeat
-            f.Readln(current);
+function TFileStrings.LoadList(var f: TFile; out s: TStringArray): fileint;
+begin
+   s := nil;
+   Result := eNONE;
 
-            if(f.Error = 0) then begin
-               p.Store(current, index);
-               inc(index);
-            end else
-               break;
-         until f.EOF();
+   LoadList(f, s);
+   f.ReadStrings(s);
 
-         exit(totalStrings)
-      end;
+   if(f.Error = 0) then
+      exit(Length(s));
 
-      f.CloseAndDestroy();
-      exit(eFILE_READ);
+   exit(eFILE_READ);
+end;
+
+function TFileStrings.LoadPacked(const fn: string; out p: TPackedStrings): fileint;
+var
+   f: TFile;
+
+begin
+   fFile.Init(f);
+   f.Open(fn);
+   p := nil;
+
+   if(f.Error = 0) then begin
+      LoadPacked(f, p);
    end else
       Result := eFILE_OPEN;
 
@@ -94,14 +123,9 @@ begin
 
    fFile.Init(f);
    f.Open(fn);
+
    if(f.Error = 0) then begin
-      f.ReadStrings(s);
-
-      if(f.Error = 0) then
-         exit(Length(s));
-
-      f.CloseAndDestroy();
-      exit(eFILE_READ);
+      Result := LoadList(f, s);
    end else
       Result := eFILE_OPEN;
 
